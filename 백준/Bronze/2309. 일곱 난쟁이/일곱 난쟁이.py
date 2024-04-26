ac2222f65dafb8f3a53a441ebac3b1dec2159cf3
ihihiu <70042863+ihihiu@users.noreply.github.com>
2024-04-26 22:46:26 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -1,21 +1,17 @@
 from itertools import combinations
 
 array = []
-for i in range(9):
+for _ in range(9):
   array.append(int(input()))
 
-combis = list(combinations(array, 7))
-
+combi = combinations(array, 7)
 result = []
-for combi in combis:
-  sum = 0
-  for a in combi:
-    sum += a
-  if sum == 100:
-    result = list(combi)
+
+for com in combi:
+  if sum(com) == 100:
+    result = list(com)
     break
 
 result.sort()
-for a in result:
-  print(a)
-
+for i in result:
+  print(i)

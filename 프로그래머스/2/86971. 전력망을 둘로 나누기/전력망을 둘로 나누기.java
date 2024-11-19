b04c947493c85f3beb3eb7c7873e68fd2f9de835
ihihiu <70042863+ihihiu@users.noreply.github.com>
2024-11-19 16:55:33 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 6.60 ms, Memory: 69.5 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -3,43 +3,34 @@ class Solution {
     public int solution(int n, int[][] wires) {
         int answer = Integer.MAX_VALUE;
         ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-        for (int i = 0; i <= n; i++) {
-            graph.add(new ArrayList<>());
+        for (int i = 0; i <= n; i++) graph.add(new ArrayList<Integer>());
+        for (int[] x : wires) {
+            graph.get(x[0]).add(x[1]);
+            graph.get(x[1]).add(x[0]);
         }
-        for (int[] wire : wires) {
-            graph.get(wire[0]).add(wire[1]);
-            graph.get(wire[1]).add(wire[0]);
-        }
-        
-        for (int[] wire: wires) {
-            int a = wire[0];
-            int b = wire[1];
-            graph.get(a).remove(Integer.valueOf(b));
-            graph.get(b).remove(Integer.valueOf(a));
-            
+        for (int[] x : wires) {
             int[] ch = new int[n + 1];
-            Queue<Integer> queue = new LinkedList<>();
-            queue.offer(1);
-            ch[1] = 1;
-            int cnt = 1;
-            while(!queue.isEmpty()) {
-                int now = queue.poll();
-                for (int x : graph.get(now)) {
-                    if (ch[x] == 0) {
-                        ch[x] = 1;
-                        queue.offer(x);
-                        cnt++;
+            ch[x[1]] = 1;
+            Queue<Integer> queue = new ArrayDeque<>();
+            queue.offer(x[0]);
+            int cnt = 0;
+            while (!queue.isEmpty()) {
+                int size = queue.size();
+                for (int r = 0; r < size; r++) {
+                    int now = queue.poll();
+                    if (ch[now] == 1) continue;
+                    ch[now] = 1;
+                    cnt++;
+                    for (int next : graph.get(now)) {
+                        if (ch[next] == 0) {
+                            queue.offer(next);
+                        }
                     }
                 }
             }
-            int diff = Math.abs(cnt - (n - cnt));
-            answer = Math.min(answer, diff);
-            
-            graph.get(a).add(b);
-            graph.get(b).add(a);
+            answer = Math.min(answer, Math.abs(cnt - (n - cnt)));
             
         }
-        
         return answer;
     }
 }

860abaa41c6c3229fe868a9d38ed20f2920ac0f6
ihihiu <70042863+ihihiu@users.noreply.github.com>
2024-11-22 22:13:42 +0900

    [level 2] Title: 프로세스, Time: 3.83 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,30 +1,21 @@
 import java.util.*;
 class Solution {
     public int solution(int[] priorities, int location) {
-        int answer = 0;
-        Queue<int[]> Q = new LinkedList<>();
+        Queue<int[]> queue = new ArrayDeque<>();
         for (int i = 0; i < priorities.length; i++) {
-            Q.offer(new int[] {priorities[i], i});
+            queue.offer(new int[] {priorities[i], i});
         }
-        Arrays.sort(priorities);
-        
-        int idx = priorities.length - 1;
-        int max = priorities[idx];
-        while (!Q.isEmpty()) {
-            int[] now = Q.poll();
-            if (now[0] == max) {
-                if (now[1] == location) {
-                    answer = priorities.length - idx;
-                    break;           
-                }
-                idx--;
-                max = priorities[idx];
+        Integer[] p = Arrays.stream(priorities).boxed().toArray(Integer[]::new);
+        Arrays.sort(p, Collections.reverseOrder());
+        int answer = 0;
+        while (true) {
+            int[] now = queue.poll();
+            if (now[0] == p[answer]) {
+                answer++;
+                if (now[1] == location) break;
             }
-            else {
-                Q.offer(now);
-            }
+            else queue.offer(now);
         }
-        
         return answer;
     }
 }

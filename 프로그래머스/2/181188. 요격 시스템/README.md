27c4bc407b87bde0a8aaddbed8711a464da9c169
ihihiu <70042863+ihihiu@users.noreply.github.com>
2025-02-12 19:58:40 +0900

    [level 2] Title: 요격 시스템, Time: 460.14 ms, Memory: 185 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/README.md
+++ 프로그래머스/2/181188. 요격 시스템/README.md
@@ -1,10 +1,10 @@
 # [level 2] 요격 시스템 - 181188 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181188?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181188) 
 
 ### 성능 요약
 
-메모리: 188 MB, 시간: 539.20 ms
+메모리: 185 MB, 시간: 460.14 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 15일 14:46:58
+2025년 02월 12일 19:58:37
 
 ### 문제 설명
 
--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
@@ -2,21 +2,21 @@ import java.util.*;
 class Solution {
     public int solution(int[][] targets) {
         int answer = 0;
-        int n = targets.length;
         Arrays.sort(targets, (a, b) -> a[1] - b[1]);
-        int idx = 0;
-        while (idx < n){
-            answer++;
-            int end = targets[idx][1];
-            idx++;
-            while (idx < n && targets[idx][0] < end){
-                if (targets[idx][1] < end) {
-                    end = targets[idx][1];
-                }
-                idx++;
-            }
-        }
+        int idx = 0, start = -1, end = -1;
         
+        while (idx < targets.length) {
+            int[] now = targets[idx];
+            if (now[0] >= end) {
+                answer++;
+                start = now[0];
+                end = now[1];
+            }
+            else {
+                if (now[0] < start) start = now[0];
+            }
+            idx++;
+        }
         return answer;
     }
 }

4eb1efef19928f783659c41ced5b2658573144e4
ihihiu <70042863+ihihiu@users.noreply.github.com>
2025-02-13 17:52:57 +0900

    [level 2] Title: 광물 캐기, Time: 4.07 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
@@ -1,13 +1,14 @@
 import java.util.*;
 class Solution {
-    static int answer, n;
+    static int answer =  Integer.MAX_VALUE;
+    static int n;
     
-    public void dfs(int depth, int res, int[] picks, String[] minerals) {
-        if (res > answer) return;
-        else if (depth >= n || Arrays.stream(picks).sum() == 0) answer = res;
+    public void dfs(int depth, int tmp, int[] picks, String[] minerals) {
+        if (tmp > answer) return;
+        else if (depth >= n || Arrays.stream(picks).sum() == 0) answer = tmp;
         else {
             for (int i = 0; i < 3; i++) {
-                if (picks[i] > 0) {
+                if (picks[i] > 0){
                     picks[i]--;
                     int sum = 0;
                     for (int j = depth; j < Math.min(n, depth + 5); j++) {
@@ -16,13 +17,13 @@ class Solution {
                             if (minerals[j].equals("diamond")) sum += 5;
                             else sum += 1;
                         }
-                        else{
-                            if (minerals[j].equals("diamond")) sum += 25;                        
+                        else {
+                            if (minerals[j].equals("diamond")) sum += 25;
                             else if (minerals[j].equals("iron")) sum += 5;
                             else sum += 1;
                         }
                     }
-                    dfs(depth + 5, res + sum, picks, minerals);
+                    dfs(depth + 5, tmp + sum, picks, minerals);
                     picks[i]++;
                 }
             }
@@ -30,7 +31,6 @@ class Solution {
     }
     
     public int solution(int[] picks, String[] minerals) {
-        answer = Integer.MAX_VALUE;
         n = minerals.length;
         dfs(0, 0, picks, minerals);
         return answer;

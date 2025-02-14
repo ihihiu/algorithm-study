ca92b95c9835c1139a2d4f8d5565bb7a9ac7c6ff
ihihiu <70042863+ihihiu@users.noreply.github.com>
2025-02-14 14:01:45 +0900

    [level 2] Title: 리코쳇 로봇, Time: 3.78 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
@@ -1,36 +1,37 @@
 import java.util.*;
 class Solution {
     public int solution(String[] board) {
-        int answer = -1;
         int n = board.length;
         int m = board[0].length();
         int[][] ch = new int[n][m];
         int[] dx = {-1, 0, 1, 0};
         int[] dy = {0, 1, 0, -1};
-        Queue<int[]> queue = new ArrayDeque<>();
+        
+        Queue<int[]> queue = new LinkedList<>();
+        
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (board[i].charAt(j) == 'R') {
                     queue.offer(new int[]{i, j});
                     ch[i][j] = 1;
                     int L = 0;
-                    while (!queue.isEmpty()) {
+                    while(!queue.isEmpty()) {
                         L++;
                         int size = queue.size();
                         for (int r = 0; r < size; r++) {
                             int[] now = queue.poll();
-                            for (int d = 0; d < 4; d++) {
-                                int nx = now[0] + dx[d];
-                                int ny = now[1] + dy[d];
+                            for (int k = 0; k < 4; k++) {
+                                int nx = now[0] + dx[k];
+                                int ny = now[1] + dy[k];
                                 if (nx < 0 || nx >= n || ny < 0 || ny >= m || board[nx].charAt(ny) == 'D') continue;
                                 while (nx >= 0 && nx < n && ny >= 0 && ny < m && board[nx].charAt(ny) != 'D') {
-                                    nx += dx[d];
-                                    ny += dy[d];
+                                    nx += dx[k];
+                                    ny += dy[k];
                                 }
-                                nx -= dx[d];
-                                ny -= dy[d];
+                                nx -= dx[k];
+                                ny -= dy[k];
                                 if (board[nx].charAt(ny) == 'G') return L;
-                                if (ch[nx][ny] == 0){
+                                if (ch[nx][ny] == 0) {
                                     ch[nx][ny] = 1;
                                     queue.offer(new int[]{nx, ny});
                                 }
@@ -41,6 +42,6 @@ class Solution {
             }
         }
         
-        return answer;
+        return -1;
     }
 }

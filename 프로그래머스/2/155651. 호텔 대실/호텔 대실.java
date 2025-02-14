93d8c7c9edc0f1d889e8508b34ee8d47eb1dd110
ihihiu <70042863+ihihiu@users.noreply.github.com>
2025-02-14 15:22:17 +0900

    [level 2] Title: 호텔 대실, Time: 22.64 ms, Memory: 91.5 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
@@ -1,21 +1,25 @@
 import java.util.*;
 class Solution {
     public int getTime(String x) {
-        int h = Integer.parseInt(x.split(":")[0]);
-        int m = Integer.parseInt(x.split(":")[1]);
+        int h = Integer.valueOf(x.split(":")[0]);
+        int m = Integer.valueOf(x.split(":")[1]);
         return h * 60 + m;
     }
     public int solution(String[][] book_time) {
         int answer = 0;
-        LinkedList<int[]> timeList = new LinkedList<>();
+        int n = book_time.length;
+        
+        LinkedList<int[]> queue = new LinkedList<>();
         for (String[] x : book_time) {
-            timeList.offer(new int[]{getTime(x[0]), getTime(x[1])});
+            queue.offer(new int[]{getTime(x[0]), getTime(x[1])});
         }
-        Collections.sort(timeList, (a, b) -> a[0] - b[0]);
-        PriorityQueue<Integer> pQ = new PriorityQueue<>((a, b) -> a - b);
-        while (!timeList.isEmpty()) {
-            int[] now = timeList.poll();
-            while (!pQ.isEmpty() && pQ.peek() <= now[0]){
+        Collections.sort(queue, (a, b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
+        
+        PriorityQueue<Integer> pQ = new PriorityQueue<>();
+        
+        for (int i = 0; i < n; i++) {
+            int[] now = queue.poll();
+            while (!pQ.isEmpty() && pQ.peek() <= now[0]) {
                 pQ.poll();
             }
             pQ.offer(now[1] + 10);

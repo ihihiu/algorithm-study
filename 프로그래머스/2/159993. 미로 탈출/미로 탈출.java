e487870054e2dfb30d5eddf1b1316a87384213ec
ihihiu <70042863+ihihiu@users.noreply.github.com>
2025-02-14 14:46:22 +0900

    [level 2] Title: 미로 탈출, Time: 6.29 ms, Memory: 90.2 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
@@ -1,51 +1,48 @@
 import java.util.*;
 class Solution {
     public int solution(String[] maps) {
+        int answer = 0;
         int n = maps.length;
         int m = maps[0].length();
-        int[][] cost = new int[n][m];
-        int sx = 0, sy = 0, ex = 0, ey = 0, lx = 0, ly = 0;
+        int[][] dist = new int[n][m];
         int[] dx = {-1, 0, 1, 0};
         int[] dy = {0, 1, 0, -1};
+        int sx = 0, sy = 0, ex = 0, ey = 0;
+        
+        Queue<int[]> queue = new LinkedList<>();
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (maps[i].charAt(j) == 'S') {
                     sx = i;
                     sy = j;
                 }
-                else if (maps[i].charAt(j) == 'L') {
-                    lx = i;
-                    ly = j;
-                }
                 else if (maps[i].charAt(j) == 'E') {
                     ex = i;
                     ey = j;
                 }
-            }
-        }
-        Queue<int[]> queue = new ArrayDeque();
-        queue.offer(new int[]{lx, ly});
-        cost[lx][ly] = 1;
-        int L = 0;
-        while (!queue.isEmpty()) {
-            L++;
-            int size = queue.size();
-            for (int i = 0; i < size; i++) {
-                int[] now = queue.poll();
-                for (int j = 0; j < 4; j++) {
-                    int nx = now[0] + dx[j];
-                    int ny = now[1] + dy[j];
-                    if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-                    if (maps[nx].charAt(ny) != 'X' && cost[nx][ny] == 0) {
-                        cost[nx][ny] = L;
-                        queue.offer(new int[]{nx, ny});
+                else if (maps[i].charAt(j) == 'L') {
+                    int L = 0;
+                    queue.offer(new int[]{i, j});
+                    dist[i][j] = -1;
+                    while (!queue.isEmpty()) {
+                        int size = queue.size();
+                        L++;
+                        for (int r = 0; r < size; r++) {
+                            int[] now = queue.poll();
+                            for (int k = 0; k < 4; k++) {
+                                int nx = now[0] + dx[k];
+                                int ny = now[1] + dy[k];
+                                if (nx < 0 || nx >= n || ny < 0 || ny >= m || maps[nx].charAt(ny) == 'X' || dist[nx][ny] != 0) continue;
+                                dist[nx][ny] = L;
+                                queue.offer(new int[]{nx, ny});
+                            }
+                        }
                     }
                 }
             }
         }
-        if (cost[sx][sy] == 0 || cost[ex][ey] == 0) return -1;
+        if (dist[sx][sy] == 0 || dist[ex][ey] == 0) return -1;
         
-        
-        return cost[sx][sy] + cost[ex][ey];
+        return dist[sx][sy] + dist[ex][ey];
     }
 }

ff7662557b582df8cc0df63d77b21f135d3bf6cf
ihihiu <70042863+ihihiu@users.noreply.github.com>
2025-02-15 17:40:44 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 7.83 ms, Memory: 82 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
@@ -2,18 +2,16 @@ import java.util.*;
 class Solution {
     public long solution(int cap, int n, int[] deliveries, int[] pickups) {
         long answer = 0;
-        int d = 0;
-        int p = 0;
+        int dCap = 0, pCap = 0;
         for (int i = n - 1; i >= 0; i--) {
-            d -= deliveries[i];
-            p -= pickups[i];
-            while (d < 0 || p < 0) {
-                d += cap;
-                p += cap;
+            dCap -= deliveries[i];
+            pCap -= pickups[i];
+            while (dCap < 0 || pCap < 0) {
+                dCap += cap;
+                pCap += cap;
                 answer += (i + 1) * 2;
             }
         }
-        
         return answer;
     }
 }

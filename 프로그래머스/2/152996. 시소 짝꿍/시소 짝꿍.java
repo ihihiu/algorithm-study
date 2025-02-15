a78a0948da9c77f32b34fcbcd15072bb796a0669
ihihiu <70042863+ihihiu@users.noreply.github.com>
2025-02-15 14:02:46 +0900

    [level 2] Title: 시소 짝꿍, Time: 28.82 ms, Memory: 88.8 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
@@ -1,25 +1,19 @@
 import java.util.*;
 class Solution {
     public long solution(int[] weights) {
+        int n = weights.length;
         long answer = 0;
         HashMap<Long, Long> sh = new HashMap<>();
-        TreeSet<Long> set = new TreeSet<>();
         for (long x : weights) {
             sh.put(x, sh.getOrDefault(x, 0L) + 1);
-            set.add(x);
         }
-        for (long x : set) {
-            long cnt = sh.get(x);
+        
+        for (long key : sh.keySet()) {
+            long cnt = sh.get(key);
+            if (key * 3 % 2 == 0 && sh.containsKey(key * 3 / 2)) answer += (cnt * sh.get(key * 3 / 2));
+            if (sh.containsKey(key * 2)) answer += (cnt * sh.get(key * 2));
+            if (key * 4 % 3 == 0 && sh.containsKey(key * 4 / 3)) answer += (cnt * sh.get(key * 4 / 3));
             if (cnt > 1) answer += (cnt * (cnt - 1) / 2);
-            if (sh.containsKey(2 * x)){
-                answer += cnt * (sh.get(2 * x));
-            }
-            if (sh.containsKey(3 * x / 2) && (3 * x) % 2 == 0){
-                answer += cnt * (sh.get(3 * x / 2));
-            }
-            if (sh.containsKey(4 * x / 3) && (4 * x) % 3 == 0){
-                answer += cnt * (sh.get(4 * x / 3));
-            }
         }
         
         return answer;

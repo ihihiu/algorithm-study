d05bc66969b9641978d48bfe803b8048be954a25
ihihiu <70042863+ihihiu@users.noreply.github.com>
2025-02-16 15:28:44 +0900

    [level 2] Title: 점 찍기, Time: 9.51 ms, Memory: 81.9 MB -BaekjoonHub

--- 프로그래머스/2/140107. 점 찍기/점 찍기.java
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.java
@@ -2,11 +2,10 @@ import java.util.*;
 class Solution {
     public long solution(int k, int d) {
         long answer = 0;
-        for (int x = 0; x <= d; x += k) {
-            int max = (int) Math.sqrt((long) d * d - (long) x * x);
-            answer += (max / k) + 1;
+        for (long i = 0; i <= d; i += k) {
+            int j = (int) Math.sqrt((long) d * d - i * i);
+            answer += (j / k) + 1;
         }
-        
         return answer;
     }
 }

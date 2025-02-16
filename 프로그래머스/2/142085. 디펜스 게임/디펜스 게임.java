fe90a5e6809c93f7f48ab0dce18eac183846f247
ihihiu <70042863+ihihiu@users.noreply.github.com>
2025-02-16 15:07:36 +0900

    [level 2] Title: 디펜스 게임, Time: 108.95 ms, Memory: 137 MB -BaekjoonHub

--- 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
@@ -2,19 +2,18 @@ import java.util.*;
 class Solution {
     public int solution(int n, int k, int[] enemy) {
         int answer = 0;
-        int len = enemy.length;
-        int sum = 0;
+        int lt = 0, sum = 0, cnt = 0;
         PriorityQueue<Integer> pQ = new PriorityQueue<>((a, b) -> b - a);
-        for (int i = 0; i < len; i++) {
-            sum += enemy[i];
-            pQ.offer(enemy[i]);
-            while (!pQ.isEmpty() && sum > n) {
-                int x = pQ.poll();
-                sum -= x;
-                k--;
-                if (k < 0) return i;
-            }
+        for (int rt = 0; rt < enemy.length; rt++) {
+            sum += enemy[rt];
+            pQ.offer(enemy[rt]);
+            if (sum > n) {
+                if (cnt == k) return rt;
+                cnt++;
+                sum -= pQ.poll();      
+            } 
         }
-        return len;
+        
+        return enemy.length;
     }
 }

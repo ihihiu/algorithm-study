039e4df642162735b3ef9c45255c123f14ac2cf9
ihihiu <70042863+ihihiu@users.noreply.github.com>
2025-02-18 02:29:14 +0900

    [level 2] Title: 할인 행사, Time: 42.25 ms, Memory: 127 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/README.md
+++ 프로그래머스/2/131127. 할인 행사/README.md
@@ -1,10 +1,10 @@
 # [level 2] 할인 행사 - 131127 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127) 
 
 ### 성능 요약
 
-메모리: 113 MB, 시간: 49.48 ms
+메모리: 127 MB, 시간: 42.25 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 19일 00:45:09
+2025년 02월 18일 02:29:11
 
 ### 문제 설명
 
--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -2,22 +2,34 @@ import java.util.*;
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
+        int n = want.length;
         HashMap<String, Integer> sh = new HashMap<>();
-        for (int i = 0; i < 9; i++) sh.put(discount[i], sh.getOrDefault(discount[i], 0) + 1);
-        int lt = 0;
+        for (int i = 0; i < n; i++) sh.put(want[i], number[i]);
+        
+        for (int i = 0; i < 9; i++) {
+            if (!sh.containsKey(discount[i])) continue;
+            sh.put(discount[i], sh.get(discount[i]) - 1);
+        }
+        
+        int lt = 0; 
         for (int rt = 9; rt < discount.length; rt++) {
-            sh.put(discount[rt], sh.getOrDefault(discount[rt], 0) + 1);
+            if (sh.containsKey(discount[rt])) sh.put(discount[rt], sh.get(discount[rt]) - 1);
             boolean flag = true;
-            for (int i = 0; i < want.length; i++) {
-                if (!sh.containsKey(want[i]) || sh.get(want[i]) < number[i]) {
+            for (int value : sh.values()) {
+                if (value > 0) {
                     flag = false;
                     break;
                 }
             }
             if (flag) answer++;
-            sh.put(discount[lt], sh.get(discount[lt]) - 1);
+            if (sh.containsKey(discount[lt])) sh.put(discount[lt], sh.get(discount[lt]) + 1);
+            
             lt++;
         }
+        
+        
+        
+        
         
         return answer;
     }

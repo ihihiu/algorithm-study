c93d4ce3177f851a69f0a336bc8353c9131fb5ec
ihihiu <70042863+ihihiu@users.noreply.github.com>
2025-02-17 16:22:43 +0900

    [level 2] Title: 택배상자, Time: 154.65 ms, Memory: 152 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.java
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -1,28 +1,24 @@
 import java.util.*;
-
 class Solution {
     public int solution(int[] order) {
         int answer = 0;
         int n = order.length;
+        
         Stack<Integer> stack = new Stack<>();
+        
         int idx = 0;
         for (int i = 1; i <= n; i++) {
-            while (!stack.isEmpty() && stack.peek() == order[idx]) {
-                stack.pop();
-                answer++;
-                idx++;
+            stack.push(i);
+            while (!stack.isEmpty()) {
+                if (stack.peek() == order[idx]) {
+                    stack.pop();
+                    idx++;
+                    answer++;
+                } else break;
             }
-            if (i == order[idx]) {
-                answer++;
-                idx++;
-            }
-            else stack.push(i);
         }
-        while (!stack.isEmpty() && stack.peek() == order[idx]) {
-            answer++;
-            stack.pop();
-            idx++;
-        }
+        
         return answer;
+        
     }
 }

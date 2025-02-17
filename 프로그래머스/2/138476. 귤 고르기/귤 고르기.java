8cde450d20f9b1688dfcd8d04c8a19e3309e867f
ihihiu <70042863+ihihiu@users.noreply.github.com>
2025-02-17 15:31:40 +0900

    [level 2] Title: 귤 고르기, Time: 82.20 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -1,22 +1,17 @@
 import java.util.*;
 class Solution {
     public int solution(int k, int[] tangerine) {
-        int answer = 0;
         HashMap<Integer, Integer> sh = new HashMap<>();
-        LinkedList<int[]> lst = new LinkedList<>();
-        for (int x : tangerine) {
-            sh.put(x, sh.getOrDefault(x, 0) + 1);
+        for (int x : tangerine) sh.put(x, sh.getOrDefault(x, 0) + 1);
+        ArrayList<Integer> arr = new ArrayList<>();
+        for (int value : sh.values()) arr.add(value);
+        Collections.sort(arr, (a, b) -> b - a);
+        int sum = 0;
+        for (int i = 0; i < arr.size(); i++) {
+            sum += arr.get(i);
+            if (sum >= k) return i + 1;
         }
-        for (int x : sh.keySet()) {
-            lst.offer(new int[]{x, sh.get(x)});
-        }
-        Collections.sort(lst, (a, b) -> b[1] - a[1]);
-        while (!lst.isEmpty()) {
-            int[] now = lst.poll();
-            answer++;
-            k -= now[1];
-            if (k <= 0) break;
-        }
-        return answer;
+        
+        return 0;
     }
 }

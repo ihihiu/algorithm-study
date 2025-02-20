0b9b8670773672796851e7cad23d2b0aa0ae4edd
ihihiu <70042863+ihihiu@users.noreply.github.com>
2025-02-20 17:01:56 +0900

    [level 2] Title: 주차 요금 계산, Time: 12.61 ms, Memory: 91.5 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/README.md
+++ 프로그래머스/2/92341. 주차 요금 계산/README.md
@@ -1,10 +1,10 @@
 # [level 2] 주차 요금 계산 - 92341 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92341) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92341#) 
 
 ### 성능 요약
 
-메모리: 90.1 MB, 시간: 16.40 ms
+메모리: 91.5 MB, 시간: 12.61 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 19일 15:28:13
+2025년 02월 20일 17:01:56
 
 ### 문제 설명
 
--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -1,38 +1,47 @@
 import java.util.*;
 class Solution {
     public int getTime(String x) {
-        int h = Integer.parseInt(x.split(":")[0]);     
-        int m = Integer.parseInt(x.split(":")[1]);
-        return h * 60 + m;
+        int h = Integer.valueOf(x.split(":")[0]) * 60;
+        int m = Integer.valueOf(x.split(":")[1]);
+        return h + m;
     }
+    
     public int[] solution(int[] fees, String[] records) {
         HashMap<Integer, ArrayList<Integer>> sh = new HashMap<>();
-        LinkedList<int[]> res = new LinkedList<>();
         for (String x : records) {
-            int a = Integer.parseInt(x.split(" ")[1]);
-            int b = getTime(x.split(" ")[0]);
-            if (!sh.containsKey(a)) sh.put(a, new ArrayList<Integer>());
-            sh.get(a).add(b);
+            int a = getTime(x.split(" ")[0]);
+            int b = Integer.valueOf(x.split(" ")[1]);
+            sh.putIfAbsent(b, new ArrayList<>());
+            sh.get(b).add(a);
         }
+        
+        ArrayList<int[]> res = new ArrayList<>();
+        
         for (int key : sh.keySet()) {
             ArrayList<Integer> arr = sh.get(key);
-            if (arr.size() % 2 == 1) arr.add(23 * 60 + 59);
-            int sum = 0, price = fees[1];
+            
+            Collections.sort(arr, (a, b) -> a - b);
+            if (arr.size() % 2 == 1) arr.add(1439);
+            
+            int total = 0;
             for (int i = 0; i < arr.size(); i += 2) {
-                sum += arr.get(i + 1) - arr.get(i);
+                int a = arr.get(i);
+                int b = arr.get(i + 1);
+                total += (b - a);
             }
-            if (sum > fees[0]) {
-                sum -= fees[0];
-                price += (sum / fees[2] * fees[3]);
-                if (sum % fees[2] != 0) price += fees[3];
+            if (total <= fees[0]) res.add(new int[] {key, fees[1]});
+            else {
+                total -= fees[0];
+                int k = total / fees[2];
+                if (total % fees[2] != 0) k++;
+                res.add(new int[] {key, fees[1] + k * fees[3]});
             }
-            res.add(new int[]{key, price});
         }
+        
         Collections.sort(res, (a, b) -> a[0] - b[0]);
         int[] answer = new int[res.size()];
-        for (int i = 0; i < res.size(); i++) {
-            answer[i] = res.get(i)[1];
-        }
+        for (int i = 0; i < res.size(); i++) answer[i] = res.get(i)[1];
+        
         return answer;
     }
 }

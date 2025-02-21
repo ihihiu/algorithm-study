16507e855ead080ddb79d7df5db1dce90a17d106
ihihiu <70042863+ihihiu@users.noreply.github.com>
2025-02-21 17:19:08 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.33 ms, Memory: 88.3 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -1,45 +1,51 @@
 import java.util.*;
 class Solution {
-    public int[] solution(String[][] places) {
-        int n = places.length;
-        int[] answer = new int[n];
+    public boolean bfs(String[] board, int i, int j) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{i, j});
+        int[][] ch = new int[5][5];
+        ch[i][j] = 1;
         int[] dx = {-1, 0, 1, 0};
         int[] dy = {0, 1, 0, -1};
-        int idx = 0;
-        for (String[] x : places) {
-            int[][] cost = new int[5][5];
-            boolean flag = true;
-            for (int i = 0; i < 5; i++) {
-                for (int j = 0; j < 5; j++) {
-                    if (flag && x[i].charAt(j) == 'P') {
-                        Queue<int[]> queue = new ArrayDeque<>();
-                        queue.offer(new int[]{i, j});
-                        int[][] ch = new int[5][5];
-                        ch[i][j] = 1;
-                        for (int r = 0; r < 2; r++) {
-                            int size = queue.size();
-                            for (int k = 0; k < size; k++) {
-                                int[] now = queue.poll();
-                                for (int d = 0; d < 4; d++) {
-                                    int nx = now[0] + dx[d];
-                                    int ny = now[1] + dy[d];
-                                    if (nx < 0 || nx >= 5 || ny < 0 || ny >= 5 || x[nx].charAt(ny) == 'X' || ch[nx][ny] == 1) continue;
-                                    if (x[nx].charAt(ny) == 'P') {
-                                        flag = false;
-                                        break;
-                                    }
-                                    if (ch[nx][ny] == 0) {
-                                        ch[nx][ny] = 1;
-                                        queue.offer(new int[]{nx, ny});
-                                    }
-                                }
-                            }
-                        }
+    
+        int round = 0;
+        while (!queue.isEmpty() && round < 2) {
+            round++;
+            int size = queue.size();
+            for (int k = 0; k < size; k++) {
+                int[] now = queue.poll();
+                for (int d = 0; d < 4; d++) {
+                    int nx = now[0] + dx[d];
+                    int ny = now[1] + dy[d];
+                    if (nx < 0 || nx >= 5 || ny < 0 || ny >= 5 || board[nx].charAt(ny) == 'X' || ch[nx][ny] == 1) continue;
+                    if (board[nx].charAt(ny) == 'P') {
+                        return false;
                     }
+                    queue.offer(new int[]{nx, ny});
+                    ch[nx][ny] = 1;
                 }
             }
-            if (flag) answer[idx] = 1;
-            idx++;
+        }
+        
+        return true;
+    }
+    
+    public int[] solution(String[][] places) {
+        int[] answer = new int[5];
+        Arrays.fill(answer, 1);
+
+        
+        for (int idx = 0; idx < 5; idx++) {
+            String[] board = places[idx];
+            for (int i = 0; i < 5; i++) {
+                for (int j = 0; j < 5; j++) {
+                    if (board[i].charAt(j) == 'P') {
+                        if(!bfs(board, i, j)) answer[idx] = 0; 
+                    }
+                }
+         
+            }
+            
         }
         return answer;
     }

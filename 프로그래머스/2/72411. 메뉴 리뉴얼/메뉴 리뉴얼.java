645746e56becfef0c6166481cb9502f738def0a5
ihihiu <70042863+ihihiu@users.noreply.github.com>
2025-02-23 18:15:27 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 29.84 ms, Memory: 99.7 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
@@ -1,42 +1,45 @@
 import java.util.*;
 class Solution {
-    public void dfs(int depth, int start, int x, String str, HashMap<String, Integer> sh, String tmp) {
-        if (depth == x) {
-            sh.put(tmp, sh.getOrDefault(tmp, 0) + 1);
+    static HashMap<Integer, HashMap<String, Integer>> sh;
+    static int[] max;
+    
+    public void dfs(int depth, int start, String str, String tmp, int[] course) {
+        if (depth > course[course.length - 1] || depth > str.length()) return;
+        if (Arrays.stream(course).anyMatch(x -> x == depth)) {
+            sh.get(depth).put(tmp, sh.get(depth).getOrDefault(tmp, 0) + 1);
+            int value = sh.get(depth).get(tmp);
+            if (value >= 2) max[depth] = Math.max(max[depth], value);
         }
-        else {
-            for (int i = start; i < str.length(); i++) {
-                dfs(depth + 1, i + 1, x,  str, sh, tmp + str.charAt(i));
-            }
+        for (int i = start; i < str.length(); i++) {
+            dfs(depth + 1, i + 1, str, tmp + str.charAt(i), course);
         }
+
     }
     
-    public ArrayList<String> solution(String[] orders, int[] course) {
-        ArrayList<String> answer = new ArrayList<>();
-        for (int x : course) {
-            HashMap<String, Integer> sh = new HashMap<>();
-            for (String y : orders) {
-                if (y.length() < x) continue;
-                char[] s = y.toCharArray();
-                Arrays.sort(s);
-                String str = "";
-                for (char c : s) str += c;
-                dfs(0, 0, x, str, sh, "");
-            }
-            ArrayList<Integer> cnt = new ArrayList<>(sh.values());
-            int max = 0;
-            for (int c : cnt) {
-                max = Math.max(max, c);
-            }
-
-            if (max >= 2) {
-                for (String key : sh.keySet()) {
-                    if (sh.get(key) == max) answer.add(key);
-                }
+    public String[] solution(String[] orders, int[] course) {        
+        sh = new HashMap<>(); 
+        max = new int[11];
+        for (int c : course) sh.put(c, new HashMap<String, Integer>());
+        
+        for (String str : orders) {
+            if (str.length() >= course[0]) {
+                char[] arr = str.toCharArray();
+                Arrays.sort(arr);
+                dfs(0, 0, String.valueOf(arr), "", course);
             }
         }
-        
-        Collections.sort(answer, (a, b) -> a.compareTo(b));
+        ArrayList<String> res = new ArrayList<>();
+        for (int key: sh.keySet()) {
+            if (max[key] == 0 || sh.get(key).isEmpty()) continue;
+            for (String value : sh.get(key).keySet()) {
+                if (sh.get(key).get(value) == max[key]) res.add(value);
+            }
+        }
+        Collections.sort(res);
+        String[] answer = new String[res.size()];
+        for (int i = 0; i < res.size(); i++) answer[i] = res.get(i);
+    
+     
         return answer;
     }
 }

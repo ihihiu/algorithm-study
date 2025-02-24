9e061f668f89ef56bc492d4aa8ccba31d2a61e78
ihihiu <70042863+ihihiu@users.noreply.github.com>
2025-02-24 15:24:57 +0900

    [level 2] Title: 튜플, Time: 115.63 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -1,21 +1,37 @@
 import java.util.*;
 class Solution {
-    public ArrayList<Integer> solution(String s) {
-        ArrayList<Integer> answer = new ArrayList<>();
-        s = s.substring(2, s.length() - 2).replace("},{", "-");
-        String[] str = s.split("-");
-        Arrays.sort(str, (a, b) -> a.length() - b.length());
-        for(String x : str) {
-            String[] tmp = x.split(",");
-            for (String y : tmp) {
-                int num = Integer.parseInt(y);
-                if (!answer.contains(num)) {
-                    answer.add(num);
+    public int[] solution(String s) {
+        ArrayList<ArrayList<Integer>> arr = new ArrayList<ArrayList<Integer>>();
+        for (int i = 0; i < s.length(); i++) {
+            ArrayList<Integer> tmp = new ArrayList<>();
+            String now = "";
+            while (s.charAt(i) != '}') {
+                if (s.charAt(i) == ',') {
+                    tmp.add(Integer.valueOf(now));
+                    now = "";
+                } 
+                else if(s.charAt(i) != '{') {
+                    now += s.charAt(i);
+                }
+                i++;
+            }
+            tmp.add(Integer.valueOf(now));
+            i++;
+            if (!tmp.isEmpty()) arr.add(tmp);
+        }
+       
+        Collections.sort(arr, (a, b) -> a.size() - b.size());
+        int[] answer = new int[arr.size()];
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < arr.size(); i++) {
+            for (int value : arr.get(i)) {
+                if (!set.contains(value)) {
+                    set.add(value);
+                    answer[i] = value;
                     break;
                 }
             }
         }
-        
         return answer;
     }
 }

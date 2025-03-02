601669256ddfbb7661f0ca9b788163a9b007faff
ihihiu <70042863+ihihiu@users.noreply.github.com>
2025-03-02 23:21:41 +0900

    [level 2] Title: 과제 진행하기, Time: 12.00 ms, Memory: 92.4 MB -BaekjoonHub

--- 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.java
+++ 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.java
@@ -1,47 +1,51 @@
 import java.util.*;
 class Solution {
     public int getTime(String x) {
-        int h = Integer.parseInt(x.split(":")[0]);
-        int m = Integer.parseInt(x.split(":")[1]);
+        int h = Integer.valueOf(x.split(":")[0]);
+        int m = Integer.valueOf(x.split(":")[1]);
         return h * 60 + m;
     }
     public String[] solution(String[][] plans) {
         int n = plans.length;
         String[] answer = new String[n];
-        LinkedList<int[]> planList = new LinkedList<>();
+        HashMap<Integer, String> sh = new HashMap<>();
+        LinkedList<int[]> info = new LinkedList<>();
         for (int i = 0; i < n; i++) {
-            planList.offer(new int[]{i, getTime(plans[i][1]), Integer.parseInt(plans[i][2])});
+            String[] x = plans[i];
+            sh.put(i, x[0]);
+            int a = getTime(x[1]);
+            int b = Integer.valueOf(x[2]);
+            info.add(new int[]{a, b, i});
         }
-        Collections.sort(planList, (a, b) -> a[1] - b[1]);
-        
-        PriorityQueue<int[]> pQ = new PriorityQueue<>((a, b) -> b[3] - a[3]);
-        int[] p = planList.poll();
-        int order = 0;
-        pQ.offer(new int[]{p[0], p[1], p[2], order++});
-        int endTime = p[1] + p[2];
-        int idx = 0;
-        while (!planList.isEmpty()) {
-            int[] now = planList.poll();
-            while (!pQ.isEmpty() && now[1] >= endTime) {
-                int[] x = pQ.poll();
-                answer[idx++] = plans[x[0]][0];
-                if (pQ.isEmpty()) break;
+        Collections.sort(info, (a, b) -> a[0] - b[0]);
+        int ansIdx = 0, idx = 0;
+        int endTime = 0;
+        Stack<int[]> stack = new Stack<>();
+        while (!info.isEmpty() || !stack.isEmpty()) {
+            if (info.isEmpty()) {
+                answer[ansIdx++] = sh.get(stack.pop()[2]);
+            }
+            else if (stack.isEmpty()) {
+                stack.push(info.poll());
+                endTime = stack.peek()[0];
+            }
+            else {
+                if (endTime + stack.peek()[1] > info.peek()[0]) {
+                    stack.peek()[1] -= info.peek()[0] - endTime;
+                    stack.push(info.poll());
+                    endTime = stack.peek()[0];
+                }
                 else {
-                    int[] y = pQ.poll();
-                    pQ.offer(new int[]{y[0], endTime, y[2], y[3]});
-                    endTime += pQ.peek()[2];
+                    endTime += stack.peek()[1];
+                    answer[ansIdx++] = sh.get(stack.pop()[2]);
                 }
             }
-            if (!pQ.isEmpty()) {
-                int[] x = pQ.poll();
-                pQ.offer(new int[]{x[0], now[1], endTime - now[1], order++});
-            }
-            pQ.offer(new int[]{now[0], now[1], now[2], order++});
-            endTime = now[1] + now[2];
+            
+          
         }
-        while (!pQ.isEmpty()) {
-            answer[idx++] = plans[pQ.poll()[0]][0];
-        }
+        
+        
+        
         
         
         return answer;

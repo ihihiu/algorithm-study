5a26d6d7094c9f6f19bd230410e6aa717ca26815
ihihiu <70042863+ihihiu@users.noreply.github.com>
2025-03-06 16:21:57 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 13.93 ms, Memory: 90.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.java
@@ -0,0 +1,29 @@
+class Solution
+{
+    public int solution(int [][]board)
+    {
+        int answer = 0;
+        int n = board.length;
+        int m = board[0].length;
+        
+        if (n == 1 || m == 1) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    if (board[i][j] == 1) return 1;
+                }
+            }
+        }
+                
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                if (board[i][j] != 0) {
+                    board[i][j] = Math.min(board[i - 1][j - 1], Math.min(board[i-1][j], board[i][j - 1])) + 1;
+                }
+                if (answer < board[i][j]) answer = board[i][j];
+            }
+        }
+        
+       
+        return answer * answer;
+    }
+}

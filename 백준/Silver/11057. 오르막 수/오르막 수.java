5ca938ecc7d4115ff4d3074e891f4660650de3f3
ihihiu <70042863+ihihiu@users.noreply.github.com>
2025-03-23 16:23:27 +0900

    [Silver I] Title: 오르막 수, Time: 180 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Silver/11057. 오르막 수/오르막 수.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[][] dp = new int[n + 1][10];
+
+        for (int i = 0; i < 10; i++) {
+            dp[1][i] = 1;
+        }
+
+        for (int i = 2; i <= n; i++) {
+            for (int j = 0; j < 10; j++) {
+                for (int k = 0; k <= j; k++) {
+                    dp[i][j] += dp[i - 1][k];
+                    dp[i][j] %= 10007;
+                }
+            }
+        }
+        int sum = 0;
+        for (int i = 0; i < 10; i++) {
+            sum += dp[n][i];
+        }
+        System.out.println(sum % 10007);
+    }
+}

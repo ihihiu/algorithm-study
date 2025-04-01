e51cf80fbceaf7450df7d78b759de4db8106bba7
ihihiu <70042863+ihihiu@users.noreply.github.com>
2025-04-01 20:26:21 +0900

    [level 2] Title: 광물 캐기, Time: 1.28 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
@@ -1,38 +1,48 @@
 import java.util.*;
 class Solution {
-    static int answer =  Integer.MAX_VALUE;
-    static int n;
+    static int answer;
+    static int n, end;
     
-    public void dfs(int depth, int tmp, int[] picks, String[] minerals) {
-        if (tmp > answer) return;
-        else if (depth >= n || Arrays.stream(picks).sum() == 0) answer = tmp;
+    public void dfs(int depth, int[] picks, String[] minerals, int sum) {
+        if (sum > answer) return;
+        if (depth == end) {
+            answer = Math.min(answer, sum);
+            return;
+        }
         else {
             for (int i = 0; i < 3; i++) {
-                if (picks[i] > 0){
-                    picks[i]--;
-                    int sum = 0;
-                    for (int j = depth; j < Math.min(n, depth + 5); j++) {
-                        if (i == 0) sum += 1;
-                        else if (i == 1) {
-                            if (minerals[j].equals("diamond")) sum += 5;
-                            else sum += 1;
-                        }
-                        else {
-                            if (minerals[j].equals("diamond")) sum += 25;
-                            else if (minerals[j].equals("iron")) sum += 5;
-                            else sum += 1;
-                        }
+                if (picks[i] == 0) continue;
+                picks[i]--;
+                int tmp = 0;
+                for (int j = depth * 5; j < Math.min(depth * 5 + 5, n); j++) {
+                    if (i == 0) {
+                        tmp++;
                     }
-                    dfs(depth + 5, tmp + sum, picks, minerals);
-                    picks[i]++;
+                    else if (i == 1) {
+                        if (minerals[j].equals("diamond")) tmp += 5;
+                        else tmp++;
+                    }
+                    else {
+                        if (minerals[j].equals("diamond")) tmp += 25;
+                        else if (minerals[j].equals("iron")) tmp += 5;
+                        else tmp++;
+                    }                
                 }
+                dfs(depth + 1, picks, minerals, sum + tmp);
+                picks[i]++;    
             }
         }
+        
     }
     
     public int solution(int[] picks, String[] minerals) {
+        answer = Integer.MAX_VALUE;
         n = minerals.length;
-        dfs(0, 0, picks, minerals);
+        end = n % 5 == 0 ? n / 5 : n / 5 + 1;
+        int p = 0;
+        for (int x : picks) p += x;
+        end = Math.min(end, p);
+        dfs(0, picks, minerals, 0);
         return answer;
     }
 }

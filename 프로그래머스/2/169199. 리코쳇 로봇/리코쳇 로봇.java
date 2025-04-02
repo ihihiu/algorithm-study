2fc875b7ac2420cb7d1fabfe0984b9ed3cfc0066
ihihiu <70042863+ihihiu@users.noreply.github.com>
2025-04-02 22:15:55 +0900

    [level 2] Title: 리코쳇 로봇, Time: 4.94 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
@@ -1,23 +1,24 @@
 import java.util.*;
 class Solution {
     public int solution(String[] board) {
+        int answer = -1;
         int n = board.length;
         int m = board[0].length();
-        int[][] ch = new int[n][m];
+        
         int[] dx = {-1, 0, 1, 0};
         int[] dy = {0, 1, 0, -1};
         
-        Queue<int[]> queue = new LinkedList<>();
-        
+        int[][] ch = new int[n][m];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (board[i].charAt(j) == 'R') {
+                    LinkedList<int[]> queue = new LinkedList<>();
                     queue.offer(new int[]{i, j});
                     ch[i][j] = 1;
                     int L = 0;
-                    while(!queue.isEmpty()) {
-                        L++;
+                    while (!queue.isEmpty()) {
                         int size = queue.size();
+                        L++;
                         for (int r = 0; r < size; r++) {
                             int[] now = queue.poll();
                             for (int k = 0; k < 4; k++) {
@@ -30,11 +31,14 @@ class Solution {
                                 }
                                 nx -= dx[k];
                                 ny -= dy[k];
-                                if (board[nx].charAt(ny) == 'G') return L;
                                 if (ch[nx][ny] == 0) {
                                     ch[nx][ny] = 1;
+                                    if (board[nx].charAt(ny) == 'G') {
+                                        return L;
+                                    }
                                     queue.offer(new int[]{nx, ny});
                                 }
+                                
                             }
                         }
                     }
@@ -42,6 +46,7 @@ class Solution {
             }
         }
         
-        return -1;
+        
+        return answer;
     }
 }

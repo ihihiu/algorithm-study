c2bf546ced00f117ea3b20dadee97e26be35cfa4
ihihiu <70042863+ihihiu@users.noreply.github.com>
2025-04-11 17:01:02 +0900

    [level 2] Title: 무인도 여행, Time: 5.80 ms, Memory: 87.9 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
@@ -1,42 +1,46 @@
 import java.util.*;
 class Solution {
+    static int n, m;
+    static int tmp;
+    static int[][] ch;
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    
+    public void dfs(int x, int y, String[] maps) {
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (nx < 0 || nx >= n || ny < 0 || ny >= m || maps[nx].charAt(ny) == 'X' || ch[nx][ny] == 1) continue;
+            tmp += maps[nx].charAt(ny) - '0';
+            ch[nx][ny] = 1;
+            dfs(nx, ny, maps);
+        }
+    }
+    
     public int[] solution(String[] maps) {
-        int n = maps.length;
-        int m = maps[0].length();
-        int[][] ch = new int[n][m];
-        int[] dx = {-1, 0, 1, 0};
-        int[] dy = {0, 1, 0, -1};
-        ArrayList<Integer> res = new ArrayList<>();
+        n = maps.length;
+        m = maps[0].length();
         
+        ArrayList<Integer> arr = new ArrayList<>();
+        
+        ch = new int[n][m];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (maps[i].charAt(j) != 'X' && ch[i][j] == 0) {
-                    int sum = maps[i].charAt(j) - '0';
-                    Queue<int[]> queue = new LinkedList<>();
-                    queue.offer(new int[]{i, j});
+                    tmp = maps[i].charAt(j) - '0';
                     ch[i][j] = 1;
-                    while (!queue.isEmpty()) {
-                        int size = queue.size();
-                        for (int r = 0; r < size; r++) {
-                            int[] now = queue.poll();
-                            for (int k = 0; k < 4; k++) {
-                                int nx = now[0] + dx[k];
-                                int ny = now[1] + dy[k];
-                                if (nx < 0 || nx >= n || ny < 0 || ny >= m || maps[nx].charAt(ny) == 'X' || ch[nx][ny] == 1) continue;
-                                sum += (maps[nx].charAt(ny)) - '0';
-                                ch[nx][ny] = 1;
-                                queue.offer(new int[]{nx, ny});
-                            }
-                        }
-                    }
-                    res.add(sum);
+                    dfs(i, j, maps);
+                    arr.add(tmp);
                 }
             }
         }
-        if (res.isEmpty()) return new int[]{-1};
-        Collections.sort(res);
-        int[] answer = new int[res.size()];
-        for (int i = 0; i < res.size(); i++) answer[i] = res.get(i);
+        
+        if (arr.size() == 0) return new int[]{-1};
+        Collections.sort(arr);
+        int[] answer = new int[arr.size()];
+        for (int i = 0; i < arr.size(); i++) answer[i] = arr.get(i);
+        
+        
         return answer;
     }
 }

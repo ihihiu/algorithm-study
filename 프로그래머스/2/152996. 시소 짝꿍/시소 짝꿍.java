115f75b4f63f4d71f9ebae8e471dcfc8a8c51f64
ihihiu <70042863+ihihiu@users.noreply.github.com>
2025-04-13 15:42:21 +0900

    [level 2] Title: 시소 짝꿍, Time: 32.25 ms, Memory: 88.6 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
@@ -1,21 +1,26 @@
 import java.util.*;
 class Solution {
     public long solution(int[] weights) {
-        int n = weights.length;
         long answer = 0;
         HashMap<Long, Long> sh = new HashMap<>();
         for (long x : weights) {
             sh.put(x, sh.getOrDefault(x, 0L) + 1);
         }
-        
-        for (long key : sh.keySet()) {
-            long cnt = sh.get(key);
-            if (key * 3 % 2 == 0 && sh.containsKey(key * 3 / 2)) answer += (cnt * sh.get(key * 3 / 2));
-            if (sh.containsKey(key * 2)) answer += (cnt * sh.get(key * 2));
-            if (key * 4 % 3 == 0 && sh.containsKey(key * 4 / 3)) answer += (cnt * sh.get(key * 4 / 3));
-            if (cnt > 1) answer += (cnt * (cnt - 1) / 2);
+        for (long x : sh.keySet()) {
+            long cnt = sh.get(x);
+            if (cnt != 1) {
+                answer += (cnt * (cnt - 1) / 2);
+            }
+            if (sh.containsKey(x * 2)) {
+                answer += (cnt * sh.get(x * 2));
+            }
+            if (x % 2 == 0 && sh.containsKey(x * 3 / 2)) {
+                answer += (cnt * sh.get(x * 3 / 2));
+            }
+            if (x % 3 == 0 && sh.containsKey(x * 4 / 3)) {
+                answer += (cnt * sh.get(x * 4 / 3));
+            }
         }
-        
         return answer;
     }
 }

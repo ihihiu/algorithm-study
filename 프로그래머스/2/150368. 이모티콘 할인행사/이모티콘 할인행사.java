6724298a9d3a4104a7983c23733deade64892261
ihihiu <70042863+ihihiu@users.noreply.github.com>
2025-04-22 14:57:26 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 50.10 ms, Memory: 93.3 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
@@ -1,47 +1,41 @@
 import java.util.*;
 class Solution {
-    static int joiner = 0;
-    static int price = 0;
-    static int[] percent = {10, 20, 30, 40};
-    static int[] tmp;
+    static ArrayList<int[]> arr;
+    static int[] salePercent = {10, 20, 30, 40};
     
-    public void dfs(int depth, int[][] users, int[] emoticons) {
+    static void dfs(int depth, int[] price, int[][] users, int[] emoticons) {
         if (depth == emoticons.length) {
             int cnt = 0, sum = 0;
-            
-            for (int[] user : users) {
-                int cost = 0;
-                boolean flag = false;
-                for (int i = 0; i < emoticons.length; i++) {
-                    if (tmp[i] >= user[0]) {
-                        cost += emoticons[i] * (100 - tmp[i]) / 100;
-                        if (cost >= user[1]) {
-                            flag = true;
-                            cnt++;
-                            break;
-                        }
-                    }
-                }
-                if (!flag) sum += cost;
+            for (int i = 0; i < price.length; i++) {
+                if (price[i] >= users[i][1]) cnt++;
+                else sum += price[i];
             }
-            if (cnt > joiner) {
-                joiner = cnt;
-                price = sum;
-            }
-            else if (joiner == cnt && sum > price) price = sum;
+            arr.add(new int[]{cnt, sum});
             return;
         }
         else {
-            for (int i = 0; i < 4; i++) {
-                tmp[depth] = percent[i];
-                dfs(depth + 1, users, emoticons);
-            }
+           for (int percent : salePercent) {
+               int[] tmp = Arrays.copyOf(price, price.length);
+               for (int i = 0; i < tmp.length; i++) {
+                   if (users[i][0] <= percent) {
+                       tmp[i] += emoticons[depth] * (100 - percent) / 100;
+                   }
+               }
+               dfs(depth + 1, tmp, users, emoticons);
+               
+           }
+                 
         }
     }
     
     public int[] solution(int[][] users, int[] emoticons) {
-        tmp = new int[emoticons.length];
-        dfs(0, users, emoticons);
-        return new int[]{joiner, price};
+        int[] answer = new int[2];
+        int[] price = new int[users.length];
+        arr = new ArrayList<>();
+        dfs(0, price, users, emoticons);
+        Collections.sort(arr, (a, b) -> a[0] == b[0] ? b[1] - a[1] : b[0] - a[0]);
+        answer[0] = arr.get(0)[0];
+        answer[1] = arr.get(0)[1];
+        return answer;
     }
 }

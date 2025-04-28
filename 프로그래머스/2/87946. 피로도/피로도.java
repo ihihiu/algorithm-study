9a9983b7d180950dc1fb9f5d1e892f4a1be4f4a7
ihihiu <70042863+ihihiu@users.noreply.github.com>
2025-04-28 21:29:14 +0900

    [level 2] Title: 피로도, Time: 3.03 ms, Memory: 87.4 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,28 +1,23 @@
 import java.util.*;
 class Solution {
-    static int n;
-    static int answer = -1;
+    static int answer;
     static int[] ch;
-     
     
-    public void dfs(int depth, int k, int[][] dungeons) {
-        if (depth == n) return;
-        for (int i = 0; i < n; i++) {
-            if (ch[i] == 0) {
-                if (k >= dungeons[i][0]) {
-                    ch[i] = 1;
-                    answer = Math.max(answer, depth + 1);
-                    dfs(depth + 1, k - dungeons[i][1], dungeons);
-                    ch[i] = 0;
-                }
+    static public void dfs(int depth, int k, int[][] dungeons) {
+        for (int i = 0; i < dungeons.length; i++) {
+            if (ch[i] == 0 && k >= dungeons[i][0]) {
+                ch[i] = 1;
+                answer = Math.max(answer, depth + 1);
+                dfs(depth + 1, k - dungeons[i][1], dungeons);
+                ch[i] = 0;
             }
         }
-        
+
     }
     
     public int solution(int k, int[][] dungeons) {
-        n = dungeons.length;
-        ch = new int[n];
+        answer = 0;
+        ch = new int[dungeons.length];
         dfs(0, k, dungeons);
         return answer;
     }

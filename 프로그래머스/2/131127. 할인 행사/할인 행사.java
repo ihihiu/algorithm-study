01f3cfc27e50456a60dc39364a95e5003a9879ad
ihihiu <70042863+ihihiu@users.noreply.github.com>
2025-04-28 15:22:51 +0900

    [level 2] Title: 할인 행사, Time: 42.43 ms, Memory: 125 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -2,18 +2,21 @@ import java.util.*;
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
-        int n = want.length;
-        HashMap<String, Integer> sh = new HashMap<>();
-        for (int i = 0; i < n; i++) sh.put(want[i], number[i]);
         
-        for (int i = 0; i < 9; i++) {
-            if (!sh.containsKey(discount[i])) continue;
-            sh.put(discount[i], sh.get(discount[i]) - 1);
+        HashMap<String, Integer> sh = new HashMap<>();
+        //HashMap<String, Integer> now = new HashMap<>();
+        for (int i = 0; i < want.length; i++) {
+            sh.put(want[i], number[i]);
         }
         
-        int lt = 0; 
-        for (int rt = 9; rt < discount.length; rt++) {
-            if (sh.containsKey(discount[rt])) sh.put(discount[rt], sh.get(discount[rt]) - 1);
+        for (int i = 0; i < 9; i++) {
+            if (sh.containsKey(discount[i])) sh.put(discount[i], sh.get(discount[i]) - 1);
+        }
+        
+        int lt = 0;
+        for (int i = 9; i < discount.length; i++) {
+            if (sh.containsKey(discount[i])) sh.put(discount[i], sh.get(discount[i]) - 1);
+            
             boolean flag = true;
             for (int value : sh.values()) {
                 if (value > 0) {
@@ -23,12 +26,8 @@ class Solution {
             }
             if (flag) answer++;
             if (sh.containsKey(discount[lt])) sh.put(discount[lt], sh.get(discount[lt]) + 1);
-            
             lt++;
         }
-        
-        
-        
         
         
         return answer;

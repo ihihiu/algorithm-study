3878b0ab3d8ccde71639745768333f8b8ac35b82
ihihiu <70042863+ihihiu@users.noreply.github.com>
2025-04-28 23:27:45 +0900

    [level 2] Title: 괄호 회전하기, Time: 83.42 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -3,30 +3,24 @@ class Solution {
     public int solution(String s) {
         int answer = 0;
         int n = s.length();
+        
         if (n % 2 == 1) return 0;
         for (int i = 0; i < n; i++) {
-            Stack<Character> stack = new Stack();
-            boolean flag = true;
-            for (int j = 0; j < n; j++) {
-                char now = s.charAt(j);
-                if (now == '(' || now == '{' || now == '[') stack.push(now);
-                else {
-                    if (stack.isEmpty()) {
-                        flag = false;
-                        break;
-                    }
-                    if ((stack.peek() == '(' && now == ')') || (stack.peek() == '{' && now == '}') || (stack.peek() == '[' && now == ']')) {
+            Stack<Character> stack = new Stack<>();
+            for (char x : s.toCharArray()) {
+                if (x == '(' || x == '{' || x == '[') stack.push(x);
+                else if (!stack.isEmpty()) {
+                    if ((x == ')' && stack.peek() == '(' )
+                        || (x == '}' && stack.peek() == '{')
+                        || (x == ']') && stack.peek() == '[') {
                         stack.pop();
                     }
-                    else {
-                        flag = false;
-                        break;
-                    }
+                    else stack.push(x);
                 }
+                else stack.push(x);
             }
-            s = s.substring(1, n) + s.charAt(0);
-            if (stack.isEmpty() && flag) answer++;
-            
+            if (stack.isEmpty()) answer++;
+            s = s.substring(1) + s.charAt(0);
         }
         return answer;
     }

3a56db67972584a2c4468a1ed9dfbba147aef924
ihihiu <70042863+ihihiu@users.noreply.github.com>
2025-04-29 15:09:15 +0900

    [level 2] Title: 문자열 압축, Time: 1.42 ms, Memory: 88.1 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -3,26 +3,32 @@ class Solution {
     public int solution(String s) {
         int n = s.length();
         int answer = n;
-        for (int i = n / 2; i >= 1; i--) {
-            int total = 0;
+        
+        for (int i = 1; i <= n / 2; i++) {
             String pre = s.substring(0, i);
+            int len = 0;
+            if (n % i != 0) len += n % i; 
             int cnt = 1;
-            for (int j = i; j + i <= n; j += i) {
-                if (pre.equals(s.substring(j, j + i))) cnt++;
+            for (int j = i; j < (n / i) * i; j += i) {
+                String now = s.substring(j, j + i);
+                if (now.equals(pre)) cnt++;
                 else {
-                    if (cnt != 1) total += String.valueOf(cnt).length(); 
-                    total += i;
-                    pre = s.substring(j, j + i);
+                    if (cnt != 1) {
+                        len += String.valueOf(cnt).length();
+                    }
+                    len += i;
                     cnt = 1;
-                    if (total > answer) break;
+                    pre = now;
                 }
             }
-            total += i;
-            if (cnt != 1) total += String.valueOf(cnt).length(); 
-            total += (n % i);
-            answer = Math.min(answer, total);
+            if (cnt != 1) len += String.valueOf(cnt).length();
+            len += i;
+            answer = Math.min(answer, len);
             
         }
+        
+        //System.out.print(s.substring(0, 1));
+        
         return answer;
     }
 }

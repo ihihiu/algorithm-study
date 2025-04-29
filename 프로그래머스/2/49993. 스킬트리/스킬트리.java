dfc0a33c2e525cc180249d218d9768c171abdd0e
ihihiu <70042863+ihihiu@users.noreply.github.com>
2025-04-29 15:35:59 +0900

    [level 2] Title: 스킬트리, Time: 0.16 ms, Memory: 81.7 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -2,25 +2,31 @@ import java.util.*;
 class Solution {
     public int solution(String skill, String[] skill_trees) {
         int answer = 0;
+        
+        HashMap<Integer, Character> sh = new HashMap<>();
         HashSet<Character> set = new HashSet<>();
-        for (char x : skill.toCharArray()) set.add(x);
+        for (int i = 0; i < skill.length(); i++) {
+            sh.put(i, skill.charAt(i));
+            set.add(skill.charAt(i));
+        }
+        
         for (String x : skill_trees) {
             int idx = 0;
             boolean flag = true;
-            for (char now : x.toCharArray()) {
-                if (set.contains(now)) {
-                    if (now == skill.charAt(idx)) {
-                        idx++;
-                        if (idx == skill.length()) break;
-                    }
-                    else {
+            for (int i = 0; i < x.length(); i++) {
+                char now = x.charAt(i);
+                if (!set.contains(now)) continue;
+                else {
+                    if (now != sh.get(idx)) {
                         flag = false;
                         break;
                     }
+                    else idx++;
                 }
             }
             if (flag) answer++;
         }
+        
         return answer;
     }
 }

cca36f68ad27ff70b3028fa73aaf708e06b6856e
ihihiu <70042863+ihihiu@users.noreply.github.com>
2025-04-29 21:16:32 +0900

    [level 2] Title: 튜플, Time: 114.63 ms, Memory: 116 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -1,36 +1,33 @@
 import java.util.*;
 class Solution {
     public int[] solution(String s) {
-        ArrayList<ArrayList<Integer>> arr = new ArrayList<ArrayList<Integer>>();
-        for (int i = 0; i < s.length(); i++) {
-            ArrayList<Integer> tmp = new ArrayList<>();
-            String now = "";
-            while (s.charAt(i) != '}') {
-                if (s.charAt(i) == ',') {
-                    tmp.add(Integer.valueOf(now));
-                    now = "";
-                } 
-                else if(s.charAt(i) != '{') {
-                    now += s.charAt(i);
+        int n = s.length();
+        
+        HashMap<Integer, Integer> sh = new HashMap<>();
+        
+        for (int i = 0; i < n; i++) {
+            if (!Character.isDigit(s.charAt(i))) continue;
+            else {
+                String num = "";
+                while (Character.isDigit(s.charAt(i))) {
+                    num += s.charAt(i);
+                    i++;
                 }
-                i++;
+                int number = Integer.valueOf(num);
+                sh.put(number, sh.getOrDefault(number, 0) + 1);
             }
-            tmp.add(Integer.valueOf(now));
-            i++;
-            if (!tmp.isEmpty()) arr.add(tmp);
         }
-       
-        Collections.sort(arr, (a, b) -> a.size() - b.size());
-        int[] answer = new int[arr.size()];
-        HashSet<Integer> set = new HashSet<>();
-        for (int i = 0; i < arr.size(); i++) {
-            for (int value : arr.get(i)) {
-                if (!set.contains(value)) {
-                    set.add(value);
-                    answer[i] = value;
-                    break;
-                }
-            }
+        
+        ArrayList<int[]> res = new ArrayList<>();
+        for (int num : sh.keySet()) {
+            res.add(new int[]{num, sh.get(num)});
+        }
+        
+        Collections.sort(res, (a, b) -> b[1] - a[1]);
+        
+        int[] answer = new int[res.size()];
+        for (int i = 0; i < res.size(); i++) {
+            answer[i] = res.get(i)[0];
         }
         return answer;
     }

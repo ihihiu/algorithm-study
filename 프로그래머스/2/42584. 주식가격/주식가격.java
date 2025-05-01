55cd120edc83f61ff30cf06a892ee0f4c6c32b09
ihihiu <70042863+ihihiu@users.noreply.github.com>
2025-05-01 21:26:44 +0900

    [level 2] Title: 주식가격, Time: 50.00 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -3,19 +3,24 @@ class Solution {
     public int[] solution(int[] prices) {
         int n = prices.length;
         int[] answer = new int[n];
-        PriorityQueue<int[]> pQ = new PriorityQueue<>((a, b) -> b[0] - a[0]);
+        
+        // (인덱스, 가격)
+        PriorityQueue<int[]> pQ = new PriorityQueue<>((a, b) -> b[1] - a[1]);
+        
+        
         for (int i = 0; i < n; i++) {
-            int nowPrice = prices[i];
-            while (!pQ.isEmpty() && pQ.peek()[0] > nowPrice) {
+            while (!pQ.isEmpty() && prices[i] < pQ.peek()[1]) {
                 int[] now = pQ.poll();
-                answer[now[1]] = i - now[1];
+                answer[now[0]] = i - now[0];
             }
-            pQ.offer(new int[]{nowPrice, i});
+            pQ.offer(new int[]{i, prices[i]});
         }
+        
         while (!pQ.isEmpty()) {
             int[] now = pQ.poll();
-            answer[now[1]] = n - 1 - now[1];
+            answer[now[0]] = n - 1 - now[0];
         }
+        
         
         return answer;
     }

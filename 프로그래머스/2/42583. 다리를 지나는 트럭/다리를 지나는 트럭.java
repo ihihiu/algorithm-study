10abe719ecfe9fa31a5869f53de30621192a1a2f
ihihiu <70042863+ihihiu@users.noreply.github.com>
2025-05-01 22:16:49 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 10.42 ms, Memory: 83.7 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -1,23 +1,26 @@
 import java.util.*;
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
+        int answer = 0;
         int n = truck_weights.length;
-        Queue<int[]> queue = new LinkedList<>();
-        int nowWeight = 0;
-        int i = 0, time = 0;
-        while (i < n || !queue.isEmpty()) {
-            if (!queue.isEmpty() && queue.peek()[0] == time) {
-                int[] x = queue.poll();
-                nowWeight -= x[1];
+        //(무게, 건너는 시간)
+        LinkedList<int[]> queue = new LinkedList<>();
+        
+        int sum = 0;
+        int idx = 0;
+        int time = 0;
+        while (idx < n) {
+            if (!queue.isEmpty() && time == queue.peek()[1]) {
+                sum -= queue.poll()[0];
             }
-            if (i < n && queue.size() < bridge_length && nowWeight + truck_weights[i] <= weight) {
-                queue.offer(new int[]{time + bridge_length, truck_weights[i]});
-                nowWeight += truck_weights[i];
-                i++;
+            if (idx < n && sum + truck_weights[idx] <= weight) {
+                queue.offer(new int[]{truck_weights[idx], time + bridge_length});
+                sum += truck_weights[idx];
+                idx++;
             }
             time++;
         }
         
-        return time;
+        return time + bridge_length;
     }
 }

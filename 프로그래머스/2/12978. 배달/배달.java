d62c94577f00d236e38d3e4e65362d825b015eeb
ihihiu <70042863+ihihiu@users.noreply.github.com>
2025-05-02 15:39:28 +0900

    [level 2] Title: 배달, Time: 13.21 ms, Memory: 82.5 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -1,35 +1,38 @@
 import java.util.*;
 class Solution {
     public int solution(int N, int[][] road, int K) {
+        int answer = 0;
         ArrayList<ArrayList<int[]>> graph = new ArrayList<>();
         for (int i = 0; i <= N; i++) graph.add(new ArrayList<>());
+        
         for (int[] x : road) {
             graph.get(x[0]).add(new int[]{x[1], x[2]});
             graph.get(x[1]).add(new int[]{x[0], x[2]});
         }
         
-        int[] dist = new int[N + 1];
-        Arrays.fill(dist, 500001);
-        dist[1] = 0;
+        int[] cost = new int[N + 1];
+        Arrays.fill(cost, Integer.MAX_VALUE);
+        cost[1] = 0;
         
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{1, 0});
-        HashSet<Integer> set = new HashSet<>();
-        set.add(1);
-        while (!queue.isEmpty()) {
-            int[] now = queue.poll();
-            if (now[1] > dist[now[0]]) continue;
-            for (int[] x : graph.get(now[0])) {
-                if (now[1] + x[1] < dist[x[0]]) {
-                    dist[x[0]] = now[1] + x[1];
-                    queue.offer(new int[]{x[0], dist[x[0]]});
-                    if (dist[x[0]] <= K) set.add(x[0]);
+        PriorityQueue<int[]> pQ = new PriorityQueue<>((a, b) -> a[1] - b[1]);
+        
+        pQ.offer(new int[]{1, 0});
+        
+        while (!pQ.isEmpty()) {
+            int[] now = pQ.poll();
+            for (int[] next : graph.get(now[0])) {
+                if (now[1] >= cost[next[0]]) continue;
+                if (now[1] + next[1] < cost[next[0]]) {
+                    cost[next[0]] = now[1] + next[1];
+                    pQ.offer(new int[] {next[0], cost[next[0]]});
                 }
             }
-            
         }
         
-
-        return set.size();
+        for (int i = 1; i <= N; i++) {
+            if (cost[i] <= K) answer++;
+        }
+        
+        return answer;
     }
 }

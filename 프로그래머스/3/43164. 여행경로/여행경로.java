0273ef7e523c39afbf7f4c2ad6adb825705f82e8
ihihiu <70042863+ihihiu@users.noreply.github.com>
2025-05-02 21:49:59 +0900

    [level 3] Title: 여행경로, Time: 0.50 ms, Memory: 89.5 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,38 +1,40 @@
 import java.util.*;
 class Solution {
-    static ArrayList<String> answer;
-    static int[] ch;
-    static int n;
-    static boolean find = false;
+    static String[] answer;
+    static String[] res;
+    static boolean[] ch;
+    static boolean find;
     
-    public void dfs(int depth, String now, ArrayList<String> tmp, String[][] tickets) {
+    static public void dfs(int depth, String[][] tickets, String now) {
         if (find) return;
-        if (depth == n) {
-            for (int i = 0; i < tmp.size(); i++) answer.add(tmp.get(i));
+        if (depth == tickets.length) {
+            res = answer.clone();
             find = true;
+            return;
         }
         else {
-            for (int i = 0; i < n; i++) {
-                if (tickets[i][0].equals(now) && ch[i] == 0) {
-                    ch[i] = 1;
-                    tmp.add(tickets[i][1]);
-                    dfs(depth + 1, tickets[i][1], tmp, tickets);
-                    tmp.remove(tmp.size() - 1);
-                    ch[i] = 0;
+            for (int i = 0; i < tickets.length; i++) {
+                if (!ch[i] && tickets[i][0].equals(now)) {
+                    ch[i] = true;
+                    answer[depth + 1] = tickets[i][1];
+                    dfs(depth + 1, tickets, tickets[i][1]);
+                    ch[i] = false;
                 }
             }
         }
-        
     }
     
-    public ArrayList<String> solution(String[][] tickets) {
+    public String[] solution(String[][] tickets) {
+        answer = new String[tickets.length + 1];
+        answer[0] = "ICN";
+        ch = new boolean[tickets.length];
+        
         Arrays.sort(tickets, (a, b) -> a[0].equals(b[0]) ? a[1].compareTo(b[1]) : a[0].compareTo(b[0]));
-        n = tickets.length;
-        ch = new int[n];
-        ArrayList<String> tmp = new ArrayList<>();
-        tmp.add("ICN");
-        answer = new ArrayList<>();
-        dfs(0, "ICN", tmp, tickets);
-        return answer;
+        
+        
+        
+        dfs(0, tickets, "ICN");
+        
+        return res;
     }
 }

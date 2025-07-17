7499fc1cdfe2842e41d8fc14d2994adf69db37a3
ihihiu <70042863+ihihiu@users.noreply.github.com>
2025-07-17 14:36:40 +0900

    [Silver I] Title: 문자열 교환, Time: 104 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Silver/1522. 문자열 교환/문자열 교환.java
+++ 백준/Silver/1522. 문자열 교환/문자열 교환.java
@@ -1,30 +1,42 @@
 import java.io.*;
 import java.util.*;
 
-class Main {
+class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str = br.readLine();
+        int N = str.length();
 
-        int total = 0;
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == 'a') total++;
+        int aCnt = 0;
+        for (char ch : str.toCharArray()) {
+            if (ch == 'a') {
+                aCnt++;
+            }
         }
 
+        if (aCnt == 0 || aCnt == N) {
+            System.out.println(0);
+            return;
+        }
+
+        String s = str + str;
+        char[] arr = s.toCharArray();
 
         int answer = Integer.MAX_VALUE;
+        int bCnt = 0;
 
-        for (int i = 0; i < str.length(); i++) {
-            int bCnt = 0;
-            for (int j = 0; j < total; j++) {
-                int idx = (i + j < str.length() ? i + j : i + j - str.length());
-                if (str.charAt(idx) == 'b') bCnt++;
-            }
+        for (int i = 0; i < aCnt; i++) {
+            if (arr[i] == 'b') bCnt++;
+        }
+        answer = bCnt;
+
+        for (int i = 1; i < N; i++) {
+            if (arr[i - 1] == 'b') bCnt--;
+            if (arr[i + aCnt - 1] == 'b') bCnt++;
             answer = Math.min(answer, bCnt);
         }
 
         System.out.println(answer);
-
 
     }
 }

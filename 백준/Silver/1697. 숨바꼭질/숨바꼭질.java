2af52f411e4386b9d435b65d5003355a829cfd8a
ihihiu <70042863+ihihiu@users.noreply.github.com>
2025-07-18 18:21:28 +0900

    [Silver I] Title: 숨바꼭질, Time: 136 ms, Memory: 18544 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,34 +1,45 @@
-import com.sun.source.tree.BinaryTree;
-
+import java.io.*;
 import java.util.*;
 
-class Main {
-    public static void main(String[] args) {
-        Main T = new Main();
-        Scanner kb = new Scanner(System.in);
-        int n = kb.nextInt();
-        int k = kb.nextInt();
-        int[] ch = new int[100001];
-        Queue<Integer> queue = new ArrayDeque<>();
-        queue.offer(n);
+class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+
+        int N = Integer.parseInt(input[0]);
+        int K = Integer.parseInt(input[1]);
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(N);
+
+        boolean[] visited = new boolean[100001];
+        visited[N] = true;
+
         int L = 0;
         while (!queue.isEmpty()) {
             int size = queue.size();
             for (int i = 0; i < size; i++) {
-                int now = queue.poll();
-                if (now == k) {
+                int x = queue.poll();
+                if (x == K) {
                     System.out.println(L);
-                    break;
+                    return;
                 }
-                for (int nx : new int[]{now - 1, now + 1, now * 2}) {
-                    if (nx < 0 || nx > 100000) continue;
-                    if (ch[nx] == 0) {
-                        queue.offer(nx);
-                        ch[nx] = 1;
-                    }
+                if (x - 1 >= 0 && !visited[x - 1]) {
+                    queue.offer(x - 1);
+                    visited[x - 1] = true;
+                }
+                if (x + 1 <= 100000 && !visited[x + 1]) {
+                    queue.offer(x + 1);
+                    visited[x + 1] = true;
+                }
+                if (x * 2 <= 100000 && !visited[x * 2]) {
+                    queue.offer(x * 2);
+                    visited[x * 2] = true;
                 }
             }
             L++;
         }
+
+        System.out.println(L);
     }
 }

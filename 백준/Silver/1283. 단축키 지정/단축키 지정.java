9fffa4be12f04ea1d3262c4ee96c9fa8fd10ee85
ihihiu <70042863+ihihiu@users.noreply.github.com>
2025-07-19 16:25:53 +0900

    [Silver I] Title: 단축키 지정, Time: 104 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Silver/1283. 단축키 지정/단축키 지정.java
+++ 백준/Silver/1283. 단축키 지정/단축키 지정.java
@@ -1,59 +1,51 @@
 import java.io.*;
 import java.util.*;
 
-class Main {
+class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-
-        HashSet<Character> set = new HashSet<>();
-
+        StringBuilder sb = new StringBuilder();
+        Set<Character> key = new HashSet<>();
         for (int i = 0; i < N; i++) {
-            String str = br.readLine();
-            String[] arr = str.split(" ");
-            boolean find = false;
-            StringBuilder answer = new StringBuilder();
-
+            String input = br.readLine();
+            String[] str = input.split(" ");
+            int location = -1;
             // 첫 글자 확인
-            for (int j = 0; j < arr.length; j++) {
-                if (set.isEmpty() || !set.contains(Character.toUpperCase(arr[j].charAt(0)))) {
-                    set.add(Character.toUpperCase(arr[j].charAt(0)));
-                    find = true;
-                    StringBuilder sb = new StringBuilder(arr[j]);
-                    sb.insert(0, '[');
-                    sb.insert(2, ']');
-                    arr[j] = sb.toString();
-                    for (int k = 0; k < arr.length; k++) {
-                        if (k == arr.length - 1) answer.append(arr[k]);
-                        else answer.append(arr[k]).append(" ");
-                    }
-                    System.out.println(answer);
+            for (int j = 0; j < str.length; j++) {
+                if (!key.contains(Character.toUpperCase(str[j].charAt(0)))) {
+                    key.add(Character.toUpperCase(str[j].charAt(0)));
+                    location = j;
                     break;
                 }
             }
-            if (!find) {
-                for (int j = 0; j < arr.length; j++) {
-                    for (int l = 0; l < arr[j].length(); l++) {
-                        if (!set.contains(Character.toUpperCase(arr[j].charAt(l)))) {
-                            set.add(Character.toUpperCase(arr[j].charAt(l)));
-                            find = true;
-
-                            StringBuilder sb = new StringBuilder(arr[j]);
-                            sb.insert(l, '[');
-                            sb.insert(l + 2, ']');
-                            arr[j] = sb.toString();
-
-                            for (int k = 0; k < arr.length; k++) {
-                                if (k == arr.length - 1) answer.append(arr[k]);
-                                else answer.append(arr[k]).append(" ");
-                            }
-                            System.out.println(answer);
+            if (location != -1) {
+                for (int j = 0; j < str.length; j++) {
+                    if (location == j) {
+                        sb.append('[').append(str[j].charAt(0)).append(']');
+                        for (int k = 1; k < str[j].length(); k++) {
+                            sb.append(str[j].charAt(k));
                         }
-                        if (find) break;
-                    }
+                        sb.append(' ');
+                    } else sb.append(str[j]).append(' ');
                 }
+                sb.append('\n');
             }
-            if (!find) System.out.println(str);
+            else {
+                for (int j = 0; j < input.length(); j++) {
+                    if (input.charAt(j) != ' ' && !key.contains(Character.toUpperCase(input.charAt(j)))) {
+                        sb.append('[').append(input.charAt(j)).append(']');
+                        key.add(Character.toUpperCase(input.charAt(j)));
+                        sb.append(input.substring(j + 1));
+                        break;
+                    }
+                    sb.append(input.charAt(j));
+                }
+                sb.append('\n');
+            }
+
+
         }
+        System.out.println(sb);
     }
 }

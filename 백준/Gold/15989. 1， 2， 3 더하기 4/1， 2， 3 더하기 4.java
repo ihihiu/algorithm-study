4b0e3f48658237c1507c44b9d2667e076334d614
ihihiu <70042863+ihihiu@users.noreply.github.com>
2025-07-20 13:42:48 +0900

    [Gold V] Title: 1, 2, 3 더하기 4, Time: 120 ms, Memory: 14924 KB -BaekjoonHub

--- 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.java
+++ 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.java
@@ -1,31 +1,35 @@
 import java.io.*;
 import java.util.*;
 
-class Main {
+class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-
         int[][] dp = new int[10001][4];
 
         dp[1][1] = 1;
         dp[2][1] = 1;
         dp[2][2] = 1;
         dp[3][1] = 1;
-        dp[3][2] = 1; // 1 + 2
+        dp[3][2] = 1;
         dp[3][3] = 1;
 
-        for (int i = 4; i <= 10000; i++) {
+        int[] nums = new int[n];
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(br.readLine());
+            max = Math.max(max, nums[i]);
+        }
+
+        for (int i = 4; i <= max; i++) {
             dp[i][1] = dp[i - 1][1];
             dp[i][2] = dp[i - 2][1] + dp[i - 2][2];
             dp[i][3] = dp[i - 3][1] + dp[i - 3][2] + dp[i - 3][3];
         }
 
-        for (int i = 0; i < n; i++) {
-            int num = Integer.parseInt(br.readLine());
-            System.out.println(dp[num][1] + dp[num][2] + dp[num][3]);
+        for (int x : nums) {
+            System.out.println(dp[x][1] + dp[x][2] + dp[x][3]);
         }
-
-
     }
 }

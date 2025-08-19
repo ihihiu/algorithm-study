21c36dd34cb744a244f2706072678185ac8fb32c
ihihiu <70042863+ihihiu@users.noreply.github.com>
2025-08-19 18:01:17 +0900

    [Gold IV] Title: 줄세우기, Time: 104 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Gold/2631. 줄세우기/줄세우기.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        int[] dp = new int[N];
+        Arrays.fill(dp, 1);
+
+        int cnt = 1;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] < arr[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            cnt = Math.max(cnt, dp[i]);
+        }
+        System.out.println(N - cnt);
+    }
+}
+

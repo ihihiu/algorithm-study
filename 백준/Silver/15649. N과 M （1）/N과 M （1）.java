317f8ae444ef8c3f854fb352b939a965875d9bc6
ihihiu <70042863+ihihiu@users.noreply.github.com>
2025-09-14 20:04:24 +0900

    [Silver III] Title: N과 M (1), Time: 672 ms, Memory: 42528 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,31 +1,31 @@
 import java.util.*;
-class Main {
-    static int n, m;
+import java.io.*;
+
+public class Main {
+    static int N, M;
     static boolean[] ch;
-    static void dfs(int depth, int[] tmp) {
-        if (depth == m) {
-            for (int x : tmp) System.out.print(x + " ");
-            System.out.println();
-        }
-        else {
-            for (int i = 0; i < n; i++) {
-                if(!ch[i]) {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        N = scanner.nextInt();
+        M = scanner.nextInt();
+
+        ch = new boolean[N + 1];
+
+        dfs(0, "");
+
+    }
+
+    static void dfs(int depth, String answer) {
+        if (depth == M) {
+            System.out.println(answer);
+        } else {
+            for (int i = 1; i <= N; i++) {
+                if (ch[i] == false) {
                     ch[i] = true;
-                    tmp[depth] = i + 1;
-                    dfs(depth + 1, tmp);
+                    dfs(depth + 1, answer + i + " ");
                     ch[i] = false;
                 }
             }
         }
-    }
-
-    public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner(System.in);
-        n = scanner.nextInt();
-        m = scanner.nextInt();
-        ch = new boolean[n];
-        int[] tmp = new int[m];
-        dfs(0, tmp);
-
     }
 }

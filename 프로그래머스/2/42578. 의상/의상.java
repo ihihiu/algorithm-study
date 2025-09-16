5e20e8b99f0dcdc4e2c124ccab8fbd747b9c1dcd
ihihiu <70042863+ihihiu@users.noreply.github.com>
2025-09-16 21:48:04 +0900

    [level 2] Title: 의상, Time: 0.15 ms, Memory: 88.7 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,17 +1,18 @@
 import java.util.*;
-
 class Solution {
     public int solution(String[][] clothes) {
-        HashMap<String, ArrayList<String>> sh = new HashMap<>();
-        for (String[] x : clothes) {
-            if (!sh.containsKey(x[1])) sh.put(x[1], new ArrayList<>());
-            sh.get(x[1]).add(x[0]);
+        HashMap<String, Integer> sh = new HashMap<>();
+        
+        for (int i = 0; i < clothes.length; i++) {
+            sh.put(clothes[i][1], sh.getOrDefault(clothes[i][1], 0) + 1);
         }
+        
         int answer = 1;
-        for (String key : sh.keySet()) {
-            answer *= (sh.get(key).size() + 1);            
+        
+        for (int x : sh.values()) {
+            answer *= (x + 1);
         }
-        answer--;
-        return answer;
+        
+        return answer - 1;
     }
 }

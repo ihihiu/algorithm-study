1e9fbaf67149f0bcbe6b9fee89db509226210084
ihihiu <70042863+ihihiu@users.noreply.github.com>
2025-09-24 21:13:59 +0900

    [level 2] Title: 주식가격, Time: 42.14 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,26 +1,25 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] prices) {
-        int n = prices.length;
-        int[] answer = new int[n];
+        int N = prices.length;
+        int[] answer = new int[N];
         
-        // (인덱스, 가격)
-        PriorityQueue<int[]> pQ = new PriorityQueue<>((a, b) -> b[1] - a[1]);
+        // (가격, 인덱스)
+        PriorityQueue<int[]> pQ = new PriorityQueue<>((a, b) -> b[0] - a[0]);
         
-        
-        for (int i = 0; i < n; i++) {
-            while (!pQ.isEmpty() && prices[i] < pQ.peek()[1]) {
-                int[] now = pQ.poll();
-                answer[now[0]] = i - now[0];
+        for (int i = 0; i < N; i++) {
+            int now = prices[i];
+            while (!pQ.isEmpty() && pQ.peek()[0] > now) {
+                int[] x = pQ.poll();
+                answer[x[1]] = i - x[1];
             }
-            pQ.offer(new int[]{i, prices[i]});
+            pQ.offer(new int[]{now, i});
         }
         
-        while (!pQ.isEmpty()) {
-            int[] now = pQ.poll();
-            answer[now[0]] = n - 1 - now[0];
+        while(!pQ.isEmpty()) {
+            int[] x = pQ.poll();
+            answer[x[1]] = N - x[1] - 1;
         }
-        
         
         return answer;
     }

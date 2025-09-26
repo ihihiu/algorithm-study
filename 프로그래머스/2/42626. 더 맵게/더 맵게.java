edfcd108b63766efd69dbd4d5a8728ff66d97efd
ihihiu <70042863+ihihiu@users.noreply.github.com>
2025-09-26 20:35:13 +0900

    [level 2] Title: 더 맵게, Time: 1549.49 ms, Memory: 128 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -1,20 +1,35 @@
 import java.util.*;
 class Solution {
     public int solution(int[] scoville, int K) {
-        int answer = -1;
-        PriorityQueue<Integer> pQ = new PriorityQueue<>((a, b) -> a - b);
-        for (int x : scoville) pQ.add(x);
-        if (pQ.peek() >= K) return 0;
-        for (int i = 0; i < scoville.length - 1; i++) {
-            int sco = pQ.poll() + 2 * pQ.poll();
-            if (sco >= K) {
-                if (pQ.isEmpty() || (!pQ.isEmpty() && pQ.peek() >= K)) {
-                    answer = i + 1;
-                    break;    
-                }
-            }
-            pQ.offer(sco);
+        int answer = 0;
+        
+        // 스코빌 지수 낮은 순
+        PriorityQueue<Integer> pQ = new PriorityQueue<>();
+        
+        for (int i = 0; i < scoville.length; i++) {
+            pQ.offer(scoville[i]);
         }
-        return answer;
+        
+        while (true) {
+            // 스코빌 지수 K 이상인지 확인
+            if (pQ.peek() >= K) {
+                return answer;
+            }
+
+            // 남은 원소가 1개뿐이면 종료
+            if (pQ.size() == 1) return -1;
+
+            // 두 원소 꺼내기
+            int first = pQ.poll();
+            int second = pQ.poll();
+
+            // 새로운 스코빌 지수 만들기
+            int newScoville = first + second * 2;
+            pQ.add(newScoville);
+            
+            answer++;
+        }
+        
+    
     }
 }

1c0f6c5a07a6badc1148375091cbbddb28c72d65
ihihiu <70042863+ihihiu@users.noreply.github.com>
2025-09-28 19:55:47 +0900

    [level 2] Title: H-Index, Time: 0.91 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -2,11 +2,14 @@ import java.util.*;
 class Solution {
     public int solution(int[] citations) {
         int answer = 0;
+        int n = citations.length;
+        
         Arrays.sort(citations);
+        
         for (int i = 0; i < citations.length; i++) {
-            int cnt = citations.length - i;
-            if (citations[i] >= cnt) {
-                answer = cnt;
+            int hIdx = n - i;
+            if (citations[i] >= n - i) {
+                answer = hIdx;
                 break;
             }
         }

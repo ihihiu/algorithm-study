241893f4101c060a64d867b94632740deb70202f
ihihiu <70042863+ihihiu@users.noreply.github.com>
2025-09-29 20:48:59 +0900

    [level 2] Title: 소수 찾기, Time: 25.55 ms, Memory: 86.6 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/README.md
+++ 프로그래머스/2/42839. 소수 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 소수 찾기 - 42839 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839?language=java) 
 
 ### 성능 요약
 
-메모리: 98.9 MB, 시간: 31.73 ms
+메모리: 86.6 MB, 시간: 25.55 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 28일 16:43:27
+2025년 09월 29일 20:48:56
 
 ### 문제 설명
 
--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,38 +1,43 @@
 import java.util.*;
 class Solution {
-    static HashSet<Integer> set = new HashSet<Integer>();
-    static int[] ch;
-    
-    public void dfs(int depth, String tmp, String numbers) {
-        if (depth > numbers.length()) return;
-        else {
-            for (int i = 0; i < numbers.length(); i++){
-                if (ch[i] == 0) {
-                    ch[i] = 1;
-                    set.add(Integer.valueOf(tmp + numbers.charAt(i)));
-                    dfs(depth + 1, tmp + numbers.charAt(i), numbers);
-                    ch[i] = 0;
-                }
-            }
-        }
-        
-    }
-    
+    static Set<Integer> set;
+    static int N;
+    static boolean[] ch;
     public int solution(String numbers) {
         int answer = 0;
-        ch = new int[numbers.length()];
+        set = new HashSet<>();
+        N = numbers.length();
+        ch = new boolean[N];
         dfs(0, "", numbers);
+        
         for (int x : set) {
-            if (x == 0 || x == 1) continue;
-            boolean flag = true;
-            for (int i = 2; i * i <= x; i++) {
+            if (x <= 1) continue;
+            boolean isPrime = true;
+            for (int i = 2; i <= Math.sqrt(x); i++) {
                 if (x % i == 0) {
-                    flag = false;
+                    isPrime = false;
                     break;
                 }
             }
-            if (flag) answer++;
+            if (isPrime) answer++;
         }
+        
         return answer;
+    }
+    
+    public void dfs(int depth, String tmp, String numbers) {
+        if (depth == N) {
+            return;
+        }
+        for (int i = 0; i < N; i++) {
+            if (ch[i] == false) {
+                tmp += (numbers.charAt(i) - '0');
+                set.add(Integer.parseInt(tmp));
+                ch[i] = true;
+                dfs(depth + 1, tmp, numbers);
+                ch[i] = false;
+                tmp = tmp.substring(0, tmp.length() - 1);
+            }
+        }
     }
 }

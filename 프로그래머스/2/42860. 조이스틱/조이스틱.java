fb444fb5f046c5008fec5f9f1e20ef70df25c084
ihihiu <70042863+ihihiu@users.noreply.github.com>
2025-10-06 00:09:19 +0900

    [level 2] Title: 조이스틱, Time: 0.06 ms, Memory: 86.8 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -1,18 +1,32 @@
+import java.util.*;
 class Solution {
     public int solution(String name) {
         int answer = 0;
-        int move = name.length() - 1;
-        int idx;
-        for (int i = 0; i < name.length(); i++) {
-            answer += Math.min(name.charAt(i) - 'A', 'Z' - name.charAt(i) + 1);
-            idx = i + 1;
-            while (idx < name.length() && name.charAt(idx) == 'A') {
-                idx++;
-            }
-            move = Math.min(move, i * 2 + name.length() - idx);
-            move = Math.min(move, (name.length() - idx) * 2 + i);
-            
+        int N = name.length();
+  
+        // 1. 알파벳 변경 횟수
+        for (int i = 0; i < N; i++) {
+            char c = name.charAt(i);
+            answer += Math.min(c - 'A', 'Z' - c + 1);
         }
-        return answer + move;
+        
+        // 2. 좌우 이동 최솟값
+        int min = N - 1; // 왼쪽에서부터 순차 탐색했을 때
+        for (int i = 0; i < N; i++) {
+            int next = i + 1;
+            
+            // 연속된 A 건너뛰기
+            while (next < N && name.charAt(next) == 'A') {
+                next++;
+            }
+            
+            // 왼쪽으로 되돌아가는 경우
+            int move = i + N - next + Math.min(i, N - next);
+            min = Math.min(min, move);
+        }
+        
+        answer += min;
+        
+        return answer;
     }
 }

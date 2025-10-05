b752f673c2647c8ac5a190cca9df843da17c30a5
ihihiu <70042863+ihihiu@users.noreply.github.com>
2025-10-05 15:48:59 +0900

    [level 2] Title: 모음 사전, Time: 16.81 ms, Memory: 86.5 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,26 +1,27 @@
 import java.util.*;
 class Solution {
-    static int answer;
-    static int cnt;
-    static boolean flag;
-    static String[] alpha = {"A", "E", "I", "O", "U"};
-    static public void dfs(int depth, String tmp, String word) {
-        if (tmp.equals(word)) {
+    static int answer, cnt;
+    static String target;
+    static char[] aeiou = {'A', 'E', 'I', 'O', 'U'};
+    public int solution(String word) {
+        answer = 0;
+        cnt = 0;
+        target = word;
+        dfs(0, "");
+        
+        return answer;
+    }
+    
+    static public void dfs(int depth, String tmp) {
+        if (answer != 0) return;
+        if (tmp.equals(target)) {
             answer = cnt;
             return;
         }
-        if (depth == 5) return;
-        else {
-            for (int i = 0; i < 5; i++) {
-                cnt++;
-                dfs(depth + 1, tmp + alpha[i], word);
-            }
+        if (depth == aeiou.length) return;
+        for (int i = 0; i < aeiou.length; i++) {
+            cnt++;
+            dfs(depth + 1, tmp + aeiou[i]);
         }
-    }
-    public int solution(String word) {
-        answer = 0;
-        flag = false;
-        dfs(0, "", word);
-        return answer;
     }
 }

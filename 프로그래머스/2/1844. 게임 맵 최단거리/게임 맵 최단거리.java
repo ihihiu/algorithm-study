7f6ef7353f2282ce9f1160c03f88169daac56571
ihihiu <70042863+ihihiu@users.noreply.github.com>
2025-10-10 20:32:07 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 10.06 ms, Memory: 55.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -2,34 +2,37 @@ import java.util.*;
 class Solution {
     public int solution(int[][] maps) {
         int answer = 0;
-        int n = maps.length;
-        int m = maps[0].length;
+        
+        int N = maps.length;
+        int M = maps[0].length;
         
         Queue<int[]> queue = new LinkedList<>();
+        boolean[][] visited = new boolean[N][M];
         queue.offer(new int[]{0, 0});
-        
-        boolean[][] ch = new boolean[n][m];
-        ch[0][0] = true;
+        visited[0][0] = true;
         
         int[] dx = {-1, 0, 1, 0};
         int[] dy = {0, 1, 0, -1};
         
+        
+        int L = 1;
         while (!queue.isEmpty()) {
             int size = queue.size();
-            answer++;
             for (int i = 0; i < size; i++) {
                 int[] now = queue.poll();
-                for (int j = 0; j < 4; j++) {
-                    int nx = now[0] + dx[j];
-                    int ny = now[1] + dy[j];
-                    if (nx < 0 || nx >= n || ny < 0 || ny >= m || ch[nx][ny] == true || maps[nx][ny] == 0) continue;
-                    ch[nx][ny] = true;
-                    queue.offer(new int[]{nx, ny});
+                for (int r = 0; r < 4; r++) {
+                    int x = now[0] + dx[r];
+                    int y = now[1] + dy[r];
+                    if (x < 0 || x >= N || y < 0 || y >= M || maps[x][y] == 0 || visited[x][y]) continue;
+                    queue.offer(new int[]{x, y});
+                    visited[x][y] = true;
                 }
             }
-            if (ch[n - 1][m - 1] == true) return answer + 1;
+            L++;
+            if (visited[N - 1][M - 1]) break;
         }
         
-        return -1;
+        
+        return visited[N - 1][M - 1] == false ? - 1 : L;
     }
 }

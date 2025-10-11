896f2de4353cde59dfa6671cd30315f64ced1993
ihihiu <70042863+ihihiu@users.noreply.github.com>
2025-10-11 14:47:29 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 6.94 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -2,26 +2,47 @@ import java.util.*;
 class Solution {
     public int solution(int[][] jobs) {
         int answer = 0;
-        int n = jobs.length;
-        int time = 0, cnt = 0, idx = 0;
-        Arrays.sort(jobs, (a, b) -> a[0] - b[0]);
-        PriorityQueue<int[]> pQ = new PriorityQueue<>((a, b) -> a[1] - b[1]);
-        while (cnt < n) {
-            while (idx < n && jobs[idx][0] <= time) {
-                pQ.offer(jobs[idx]);
+        int N = jobs.length;
+        
+        LinkedList<int[]> list = new LinkedList<>();
+        for (int i = 0; i < N; i++) {
+            list.add(new int[]{jobs[i][0], jobs[i][1], i});
+        }
+        
+        // (소요시간, 요청 시각, 작업 번호)
+        PriorityQueue<int[]> pQ = new PriorityQueue<>((a, b) -> {
+            if (a[0] == b[0]) {
+                if (a[1] == b[1]) {
+                    return a[2] - b[2];
+                } else return a[1] - b[1];
+            } else return a[0] - b[0];
+        });
+        
+        Collections.sort(list, (a, b) -> a[0] - b[0]);
+        
+        int time = list.get(0)[0];
+        int idx = 0;
+        
+        while (true) {
+            // 종료 조건
+            if (idx >= N && pQ.isEmpty()) break;
+            
+            while (idx < N && list.get(idx)[0] <= time) {
+                int[] x = list.get(idx);
+                pQ.add(new int[]{x[1], x[0], x[2]});
                 idx++;
             }
-            if (!pQ.isEmpty()) {
-                int[] x = pQ.poll();
-                answer += (time - x[0] + x[1]);
-                time += x[1];
-                cnt++;
+            
+            if (pQ.isEmpty() && idx < N) {
+                time = list.get(idx)[0];
             }
-            else {
-                time = jobs[idx][0];
+            else {            
+                int[] now = pQ.poll();
+                answer += (time + now[0] - now[1]);
+                time += now[0];
             }
         }
         
-        return answer / n;
+        return answer / N;
     }
 }

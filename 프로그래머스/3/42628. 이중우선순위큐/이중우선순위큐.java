2ebac3c46137e3273ed24950deb8b4c55c608412
ihihiu <70042863+ihihiu@users.noreply.github.com>
2025-10-12 14:40:29 +0900

    [level 3] Title: 이중우선순위큐, Time: 64.87 ms, Memory: 125 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -2,29 +2,40 @@ import java.util.*;
 class Solution {
     public int[] solution(String[] operations) {
         int[] answer = new int[2];
+        int N = operations.length;
+        
+        PriorityQueue<Integer> maxQ = new PriorityQueue<>((a, b) -> b - a);
+        PriorityQueue<Integer> minQ = new PriorityQueue<>();
+        
         int size = 0;
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-        for (String x : operations) {
-            char op = x.charAt(0);
-            int num = Integer.parseInt(x.split(" ")[1]);
+        
+        for (int i = 0; i < N; i++) {
+            char op = operations[i].charAt(0);
+            int num = Integer.parseInt(operations[i].split(" ")[1]);
             if (op == 'I') {
-                maxHeap.offer(num);
-                minHeap.offer(num);
+                maxQ.add(num);
+                minQ.add(num);
                 size++;
             }
             else {
-                if (size != 0){
-                    if (num == 1) minHeap.remove(maxHeap.poll());
-                    else maxHeap.remove(minHeap.poll());
+                if (size != 0) {
+                    if (num == -1) {
+                        maxQ.remove(minQ.poll());
+                    }
+                    if (num == 1) {
+                        minQ.remove(maxQ.poll());
+                    }
                     size--;
                 }
             }
-            
         }
+        
         if (size == 0) return answer;
-        answer[0] = maxHeap.poll();
-        answer[1] = minHeap.poll();
+        else {
+            answer[0] = maxQ.poll();
+            answer[1] = minQ.poll();
+        }
+        
         return answer;
     }
 }

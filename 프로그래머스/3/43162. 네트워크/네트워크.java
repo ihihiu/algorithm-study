cbca6c47bb89f0ae1356aa63ebb6971fb9578d36
ihihiu <70042863+ihihiu@users.noreply.github.com>
2025-10-17 20:49:09 +0900

    [level 3] Title: 네트워크, Time: 0.19 ms, Memory: 87.6 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,39 +1,27 @@
 import java.util.*;
 class Solution {
-    static int[] parent;
-    
-    static public int find(int x) {
-        if (x == parent[x]) return x;
-        return parent[x] = find(parent[x]);
-    }
-    
-    static public void union(int x, int y) {
-        int px = find(x);
-        int py = find(y);
-        if (px > py) {
-            parent[py] = px;
-        } else parent[px] = py;
-    }
-    
+    static boolean[] visited;
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        parent = new int[n];
-        for (int i = 0; i < n; i++) parent[i] = i;
+        visited = new boolean[n];
         
         for (int i = 0; i < n; i++) {
-            int[] row = computers[i];
-            for (int j = 0; j < row.length; j++) {
-                if (i == j) continue;
-                if (row[j] == 1) union(i, j);
+            if (!visited[i]) {
+                dfs(i, computers);
+                answer++;
             }
         }
         
-        Set<Integer> set = new HashSet<>();
-        for (int i = 0; i < n; i++) {
-            set.add(find(parent[i]));
-        }
-        answer = set.size();
-        
         return answer;
+    }
+    
+    static public void dfs(int x, int[][] computers) {
+        visited[x] = true;
+        
+        for (int i = 0; i < computers[x].length; i++) {
+            if (computers[x][i] == 1 && !visited[i]) {
+                dfs(i, computers);
+            }
+        }
     }
 }

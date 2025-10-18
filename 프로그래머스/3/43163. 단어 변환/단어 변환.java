7d1ae31dda06f4bae58c9ddc9849bc5179b7b007
ihihiu <70042863+ihihiu@users.noreply.github.com>
2025-10-18 11:23:31 +0900

    [level 3] Title: 단어 변환, Time: 0.16 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,36 +1,39 @@
 import java.util.*;
 class Solution {
+    static int N, answer;
     static boolean[] ch;
-    static int answer;
-    
-    static public void dfs(int depth, String now, String target, String[] words) {
-        if (depth >= answer) return;
-        if (now.equals(target)) {
-            answer = depth;
-            return;
-        }
-        else {
-            for (int i = 0; i < words.length; i++) {
-                if (!ch[i]) {
-                    int cnt = 0;
-                    for (int j = 0; j < target.length(); j++) {
-                        if (words[i].charAt(j) != now.charAt(j)) cnt++; 
-                    }
-                    if (cnt == 1) {
-                        ch[i] = true;
-                        dfs(depth + 1, words[i], target, words);
-                        ch[i] = false;
-                    }
-                }
-            }
-        }
-        
-    }
-    
     public int solution(String begin, String target, String[] words) {
         answer = 100;
-        ch = new boolean[words.length];
-        dfs(0, begin, target, words);
+        N = words.length;
+        ch = new boolean[N];
+        
+        dfs(0, begin, words, target);
+        
         return answer == 100 ? 0 : answer;
+    }
+    
+    static public void dfs(int depth, String now, String[] words, String target) {
+        if (now.equals(target)) {
+            answer = Math.min(answer, depth);
+        }
+        if (depth == N) return;
+        for (int i = 0; i < N; i++) {
+            if (!ch[i] && calculate(words[i], now)) {
+                ch[i] = true;
+                dfs(depth + 1, words[i], words, target);
+                ch[i] = false;
+            }
+        }
+    }
+    
+    public static boolean calculate(String now, String target) {
+        int cnt = 0;
+        for (int i = 0; i < now.length(); i++) {
+            if (now.charAt(i) != target.charAt(i)) {
+                cnt++;
+                if (cnt > 1) return false;
+            }
+        }
+        return cnt == 1 ? true : false;
     }
 }

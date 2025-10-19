f0961330710d445e97a88da99cb94226c76f8081
ihihiu <70042863+ihihiu@users.noreply.github.com>
2025-10-19 19:49:34 +0900

    [level 3] Title: 여행경로, Time: 0.53 ms, Memory: 87.7 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,40 +1,31 @@
 import java.util.*;
 class Solution {
+    static boolean flag = false;
     static String[] answer;
-    static String[] res;
     static boolean[] ch;
-    static boolean find;
-    
-    static public void dfs(int depth, String[][] tickets, String now) {
-        if (find) return;
-        if (depth == tickets.length) {
-            res = answer.clone();
-            find = true;
-            return;
-        }
-        else {
-            for (int i = 0; i < tickets.length; i++) {
-                if (!ch[i] && tickets[i][0].equals(now)) {
-                    ch[i] = true;
-                    answer[depth + 1] = tickets[i][1];
-                    dfs(depth + 1, tickets, tickets[i][1]);
-                    ch[i] = false;
-                }
-            }
-        }
-    }
-    
     public String[] solution(String[][] tickets) {
         answer = new String[tickets.length + 1];
         answer[0] = "ICN";
-        ch = new boolean[tickets.length];
-        
         Arrays.sort(tickets, (a, b) -> a[0].equals(b[0]) ? a[1].compareTo(b[1]) : a[0].compareTo(b[0]));
-        
-        
-        
-        dfs(0, tickets, "ICN");
-        
-        return res;
+        ch = new boolean[tickets.length];
+        dfs(0, "ICN", tickets);
+        return answer;
+    }
+    
+    static public void dfs(int depth, String start, String[][] tickets) {
+        if (flag) return;
+        if (depth == tickets.length) {
+            flag = true;
+            return;
+        }
+        for (int i = 0; i < tickets.length; i++) {
+            if (!ch[i] && tickets[i][0].equals(start)) {
+                answer[depth + 1] = tickets[i][1];
+                ch[i] = true;
+                dfs(depth + 1, tickets[i][1], tickets);
+                if (flag) return;
+                ch[i] = false;
+            }
+        }
     }
 }

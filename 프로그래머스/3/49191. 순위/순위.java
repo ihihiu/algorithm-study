4a816f75ab706d0950e8c1f56eefeb0daa03f03d
ihihiu <70042863+ihihiu@users.noreply.github.com>
2025-10-25 20:42:50 +0900

    [level 3] Title: 순위, Time: 10.16 ms, Memory: 93 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.java
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -1,35 +1,32 @@
+import java.util.*;
 class Solution {
     public int solution(int n, int[][] results) {
-        int[][] graph = new int[n + 1][n + 1];
-        for (int[] edge : results) {
-            graph[edge[0]][edge[1]] = 1;
-            graph[edge[1]][edge[0]] = -1;
+        int answer = 0;
+        
+        boolean[][] relation = new boolean[n + 1][n + 1];
+        
+        for (int[] result : results) {
+            relation[result[0]][result[1]] = true;
         }
         
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                for (int k = 1; k <= n; k++) {
-                    if (graph[i][k] == 1 && graph[k][j] == 1) {
-                        graph[i][j] = 1;
-                        graph[j][i] = -1;
-                    }
-                    if (graph[i][k] == -1 && graph[k][j] == -1) {
-                        graph[i][j] = -1;
-                        graph[j][i] = 1;
-                    }
-                }
+        //플로이드 워셜
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                 for (int j = 1; j <= n; j++) {
+                     if (relation[i][k] && relation[k][j]) relation[i][j] = true;
+                 }
             }
         }
         
-      
-        int answer = 0;
         for (int i = 1; i <= n; i++) {
             int cnt = 0;
             for (int j = 1; j <= n; j++) {
-                if (graph[i][j] != 0) cnt++;
+                if (i == j) continue;
+                if (relation[i][j] || relation[j][i]) cnt++;
             }
             if (cnt == n - 1) answer++;
         }
+        
         return answer;
     }
 }

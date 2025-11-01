0a2d0985bed3a8f07b9d7c05a57bf9e07afdb414
ihihiu <70042863+ihihiu@users.noreply.github.com>
2025-11-01 20:56:20 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 100 ms, Memory: 14060 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,18 +1,28 @@
 import java.util.*;
+import java.io.*;
 
-class Main {
-    public static void main(String[] args) {
-        Scanner kb = new Scanner(System.in);
-        int n = kb.nextInt();
-        int[] dp = new int[11];
-        dp[1] = 1;
-        dp[2] = 2;
-        dp[3] = 4;
-        for (int i = 4; i < 11; i++) {
-            dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1];
-        }
-        for (int i = 0; i < n; i++) {
-            System.out.println(dp[kb.nextInt()]);
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        for (int t = 0; t < T; t++) {
+            int N = Integer.parseInt(br.readLine());
+            if (N <= 3) {
+                if (N == 1) System.out.println(1);
+                else if (N == 2) System.out.println(2);
+                else System.out.println(4);
+                continue;
+            }
+            int[] dp = new int[N + 1];
+            dp[1] = 1;
+            dp[2] = 2; // 1 + 1, 2
+            dp[3] = 4; // 1 + 1 + 1, 1 + 2, 2 + 1, 3
+
+            for (int i = 4; i <= N; i++) {
+                dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+            }
+            System.out.println(dp[N]);
         }
     }
 }

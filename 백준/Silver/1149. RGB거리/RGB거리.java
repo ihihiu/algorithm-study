481668f3b4289f6aaa407ed17e3782bdadc1a5a2
ihihiu <70042863+ihihiu@users.noreply.github.com>
2025-11-02 22:11:25 +0900

    [Silver I] Title: RGB거리, Time: 120 ms, Memory: 14568 KB -BaekjoonHub

---
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[][] dp = new int[N][3];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        dp[0][0] = Integer.parseInt(st.nextToken());
+        dp[0][1] = Integer.parseInt(st.nextToken());
+        dp[0][2] = Integer.parseInt(st.nextToken());
+
+        for (int i = 1; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int R = Integer.parseInt(st.nextToken());
+            int G = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + R;
+            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + G;
+            dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + B;
+        }
+
+        System.out.println(Math.min(dp[N - 1][0], Math.min(dp[N - 1][1], dp[N - 1][2])));
+
+    }
+}

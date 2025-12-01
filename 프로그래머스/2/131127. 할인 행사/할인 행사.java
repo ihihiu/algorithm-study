f674225a763b692bf19d0d3a97cc1424a55b04dd
ihihiu <70042863+ihihiu@users.noreply.github.com>
2025-12-01 23:18:40 +0900

    [level 2] Title: 할인 행사, Time: 51.68 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -2,33 +2,29 @@ import java.util.*;
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
-        
-        HashMap<String, Integer> sh = new HashMap<>();
-        //HashMap<String, Integer> now = new HashMap<>();
+        HashMap<String, Integer> wantMap = new HashMap<>();
         for (int i = 0; i < want.length; i++) {
-            sh.put(want[i], number[i]);
+            wantMap.put(want[i], number[i]);
         }
         
+        HashMap<String, Integer> now = new HashMap<>();
         for (int i = 0; i < 9; i++) {
-            if (sh.containsKey(discount[i])) sh.put(discount[i], sh.get(discount[i]) - 1);
+            now.put(discount[i], now.getOrDefault(discount[i], 0) + 1);
         }
         
-        int lt = 0;
         for (int i = 9; i < discount.length; i++) {
-            if (sh.containsKey(discount[i])) sh.put(discount[i], sh.get(discount[i]) - 1);
-            
+            now.put(discount[i], now.getOrDefault(discount[i], 0) + 1);
             boolean flag = true;
-            for (int value : sh.values()) {
-                if (value > 0) {
+            for (String key : wantMap.keySet()) {
+                if (now.containsKey(key) && now.get(key) == wantMap.get(key)) continue;
+                else {
                     flag = false;
                     break;
                 }
             }
             if (flag) answer++;
-            if (sh.containsKey(discount[lt])) sh.put(discount[lt], sh.get(discount[lt]) + 1);
-            lt++;
+            now.put(discount[i - 9], now.getOrDefault(discount[i - 9], 0) - 1);
         }
-        
         
         return answer;
     }

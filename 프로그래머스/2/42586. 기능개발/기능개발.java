68e8aa6bac0c2a9946556e012f22d5e3a177ca0e
ihihiu <70042863+ihihiu@users.noreply.github.com>
2025-12-08 20:50:59 +0900

    [level 2] Title: 기능개발, Time: 0.05 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,27 +1,26 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        int N = progresses.length;
-        int cnt = 0;
-        int day = 0;
-        
         ArrayList<Integer> arr = new ArrayList<>();
+        int N = progresses.length;
         
+        int day = 0;
+        int cnt = 0;
         for (int i = 0; i < N; i++) {
             int x = 100 - progresses[i];
-            int d = x / speeds[i] + (x % speeds[i] == 0 ? 0 : 1);
-            if (d > day) {
-                day = d;
+            int y = x / speeds[i];
+            if (x % speeds[i] != 0) y++;
+            if (day < y) {
                 arr.add(cnt);
                 cnt = 1;
+                day =y;
             } else {
                 cnt++;
             }
         }
-        
         arr.add(cnt);
-        
         int[] answer = new int[arr.size() - 1];
+        
         for (int i = 1; i < arr.size(); i++) {
             answer[i - 1] = arr.get(i);
         }

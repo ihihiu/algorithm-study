799f023e2242e047700be9f3285d2e991100254a
ihihiu <70042863+ihihiu@users.noreply.github.com>
2025-12-14 17:42:14 +0900

    [level 2] Title: 전화번호 목록, Time: 334.63 ms, Memory: 100 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,22 +1,17 @@
 import java.util.*;
 class Solution {
     public boolean solution(String[] phone_book) {
+        boolean answer = true;
         int n = phone_book.length;
-        Arrays.sort(phone_book, (a, b) -> a.compareTo(b));
+     
+        Arrays.sort(phone_book);
         
         for (int i = 0; i < n - 1; i++) {
-            String a = phone_book[i];
-            String b = phone_book[i + 1];
-            boolean flag = true;
-            for (int j = 0; j < a.length(); j++) {
-                if (a.charAt(j) != b.charAt(j)) {
-                    flag = false;
-                    break;
-                }
+            if (phone_book[i + 1].startsWith(phone_book[i])) {
+                return false;
             }
-            if (flag) return false;
         }
         
-        return true;
+        return answer;
     }
 }

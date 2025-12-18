b3b33698d8b1eb4a09b066e4033cb239c71b72fb
ihihiu <70042863+ihihiu@users.noreply.github.com>
2025-12-18 22:17:31 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.91 ms, Memory: 55.6 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,38 +1,30 @@
 import java.util.*;
 class Solution {
     public int solution(int[][] maps) {
-        int answer = 0;
-        
         int N = maps.length;
         int M = maps[0].length;
         
         Queue<int[]> queue = new LinkedList<>();
-        boolean[][] visited = new boolean[N][M];
-        queue.offer(new int[]{0, 0});
-        visited[0][0] = true;
+        queue.add(new int[]{0, 0});
         
         int[] dx = {-1, 0, 1, 0};
         int[] dy = {0, 1, 0, -1};
         
-        
-        int L = 1;
         while (!queue.isEmpty()) {
             int size = queue.size();
             for (int i = 0; i < size; i++) {
                 int[] now = queue.poll();
-                for (int r = 0; r < 4; r++) {
-                    int x = now[0] + dx[r];
-                    int y = now[1] + dy[r];
-                    if (x < 0 || x >= N || y < 0 || y >= M || maps[x][y] == 0 || visited[x][y]) continue;
+                for (int j = 0; j < 4; j++) {
+                    int x = now[0] + dx[j];
+                    int y = now[1] + dy[j];
+                    if (x < 0 || x >= N || y < 0 || y >= M || maps[x][y] != 1) continue;
+                    maps[x][y] = maps[now[0]][now[1]] + 1;
                     queue.offer(new int[]{x, y});
-                    visited[x][y] = true;
                 }
             }
-            L++;
-            if (visited[N - 1][M - 1]) break;
         }
         
         
-        return visited[N - 1][M - 1] == false ? - 1 : L;
+        return maps[N - 1][M - 1] == 1 ? -1 : maps[N - 1][M - 1];
     }
 }
